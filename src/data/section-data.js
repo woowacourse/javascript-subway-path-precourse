b1f81d516225bf5701fb0a649e243700d2e93e9b
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-19 15:15:49 +0900

    Feat: 역, 노선, 구간 데이터를 초기화 하기

---
+++ src/data/section-data.js
@@ -0,0 +1,44 @@
+export const sections = [
+	{
+		startStation: '교대',
+		endStation: '강남',
+		distance: 2,
+		time: 3
+	},
+	{
+		startStation: '강남',
+		endStation: '역삼',
+		distance: 2,
+		time: 3
+	},
+	{
+		startStation: '교대',
+		endStation: '남부터미널',
+		distance: 3,
+		time: 2
+	},
+	{
+		startStation: '남부터미널',
+		endStation: '양재',
+		distance: 6,
+		time: 5
+	},
+	{
+		startStation: '양재',
+		endStation: '매봉',
+		distance: 1,
+		time: 1
+	},
+	{
+		startStation: '강남',
+		endStation: '양재',
+		distance: 2,
+		time: 8
+	},
+	{
+		startStation: '양재',
+		endStation: '양재시민의숲',
+		distance: 10,
+		time: 3
+	},
+]
---
+++ src/data/station-data.js
@@ -0,0 +1,23 @@
+export const stations = [
+	{
+		name: '교대'
+	},
+	{
+		name: '강남'
+	},
+	{
+		name: '역삼'
+	},
+	{
+		name: '남부터미널'
+	},
+	{
+		name: '양재'
+	},
+	{
+		name: '매봉'
+	},
+	{
+		name: '양재시민의 숲'
+	},
+]
