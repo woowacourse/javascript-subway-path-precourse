2bfc75325a7b4056b00f1383629cf76376f19621
ChayanRin <k852012@naver.com>
2020-12-19 14:06:34 +0900

    feat: checkVaild 구현

--- src/controllers/SubwayPath.js
+++ src/controllers/SubwayPath.js
@@ -1,5 +1,24 @@
+import {
+  isEnoughLength,
+  isDiffrentName,
+  isAvailable,
+  isConnected,
+} from "../utils/util.js";
+
 class SubwayPath {
   constructor() {}
+  checkVaild = (departure, arrival) => {
+    const vaild =
+      isEnoughLength(departure, arrival) ||
+      isDiffrentName(departure, arrival) ||
+      isAvailable(departure, arrival) ||
+      isConnected(departure, arrival);
+    if (vaild) {
+      alert(vaild);
+    } else {
+      return true;
+    }
+  };
 }
 
 export default SubwayPath;
