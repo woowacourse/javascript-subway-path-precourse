161ce18026af63947cb1fb1554d69f53e17d6295
dande <7bdande@gmail.com>
2020-12-19 14:05:21 +0900

    feat: [SubwayManager] 기본 DOM 요소 초기화
    DOM 요소를 불러와 초기화를 하고 버튼에 이벤트 리스너를 등록

--- src/components/SubwayManager.js
+++ src/components/SubwayManager.js
@@ -1,10 +1,41 @@
 import { stations, lines } from "../@shared/data.js";
-
+import { hideElement, showElement } from "../@shared/domUtils.js";
 class SubwayManager {
   constructor() {
     this.stations = stations;
     this.lines = lines;
+
+    this.render();
   }
+
+  setDOMElements = () => {
+    this.$departureStationInput = document.querySelector(
+      "#departure-station-name-input"
+    );
+    this.$arrivalStationInput = document.querySelector(
+      "#arrival-station-name-input"
+    );
+    this.$searchTypeRadioInputs = document.querySelectorAll(
+      "input[name='search-type']"
+    );
+    this.$searchButton = document.querySelector("#search-button");
+    this.$resultContainer = document.querySelector("#result-container");
+  };
+
+  handleSearchButton = event => {
+    event.preventDefault();
+  };
+
+  resetDOMElements = () => {
+    hideElement(this.$resultContainer);
+  };
+
+  render = () => {
+    this.setDOMElements();
+    this.resetDOMElements();
+
+    this.$searchButton.addEventListener("click", this.handleSearchButton);
+  };
 }
 
 export default SubwayManager;
