22c5707b3516572407973b6b4bf636700f00022a
jang-yunmi <newapache@naver.com>
2020-12-19 14:56:50 +0900

    feat: 출발역,도착역 간 경로가 존재하는지 확인 기능 구현

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const STATION = {
   NAME_LENGTH_LIMIT: 2,
-  INPUT_ERROR_MESSAGE: "역 이름을 다시 입력하세요.",
+  INPUT_ERROR_MESSAGE: "경로를 찾을 역 이름을 다시 입력하세요.",
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { validator } from "./utils/validator.js";
 import { findShortestPath } from "./pathFinder.js";
+import { STATION } from "./constants.js";
 
 export default class SubwayManager {
   constructor() {
@@ -15,6 +16,11 @@ export default class SubwayManager {
       .value.replace(/ /g, "");
 
     if (validator(departure, arrival)) findShortestPath(departure, arrival);
+    else {
+      alert(STATION.INPUT_ERROR_MESSAGE);
+      document.getElementById("departure-station-name-input").value = "";
+      document.getElementById("arrival-station-name-input").value = "";
+    }
   }
 
   initEvent() {
