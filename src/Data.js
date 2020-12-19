65affcc82c6f82516dd65c7e754cf41a71e93bb2
Kwakcena <rhkrgudwh@gmail.com>
2020-12-19 17:26:49 +0900

    Refactor: Move data
    * sections값은 Data에 적절하여 옮긴다.

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,15 @@
 import Dijkstra from './utils/Dijkstra.js';
 
 export const stations = ['교대', '강남', '역삼', '남부터미널', '양재', '양재시민의숲', '매봉'];
+export const sections = [
+  { start: '교대', end: '강남', distance: 2, time: 3 },
+  { start: '강남', end: '역삼', distance: 2, time: 3 },
+  { start: '교대', end: '남부터미널', distance: 3, time: 2 },
+  { start: '남부터미널', end: '양재', distance: 6, time: 5 },
+  { start: '양재', end: '매봉', distance: 1, time: 1 },
+  { start: '강남', end: '양재', distance: 2, time: 8 },
+  { start: '양재', end: '양재시민의숲', distance: 10, time: 3 },
+]
 
 export function fetchShortestDistanceDijkstra() {
   const dijkstra = new Dijkstra();
