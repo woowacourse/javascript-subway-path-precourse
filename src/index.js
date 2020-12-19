4fb1e2678774c45bf71d73acb5b9fca6013a32c7
swimme <2sy100@likelion.org>
2020-12-19 14:07:41 +0900

    feat: 경로조회 입력값 받기

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ export default class App {
     this.lines = lines;
     this.stations = stations;
   }
+
+  getDepartureStationInput() {
+    const depatureStationInputField = document.getElementById("departure-station-name-input");
+    return depatureStationInputField.value;
+  }
+
+  getArrivalStationInput() {
+    const arrivalStationInputField = document.getElementById("arrival-station-name-input");
+    return arrivalStationInputField.value;
+  }
+
+  getSearchTypeInput() {
+    const searchTypeInputField = document.querySelector('input[name="search-type"]:checked');
+    return searchTypeInputField.value;
+  }
 }
 
 const app = new App();
