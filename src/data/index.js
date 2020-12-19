fbf32ae0f6b341b96243dca112d8ae962da678c8
2SOOY <1401660@naver.com>
2020-12-19 17:17:37 +0900

    Refactor - 사용하지 않는 데이터 삭제

--- src/data/index.js
+++ src/data/index.js
@@ -22,21 +22,6 @@ export const stations = [
   },
 ];
 
-export const lines = [
-  {
-    name: "2호선",
-    stations: ["교대", "강남", "역삼"],
-  },
-  {
-    name: "3호선",
-    stations: ["교대", "남부터미널", "양재", "매봉"],
-  },
-  {
-    name: "신분당선",
-    stations: ["강남", "양재", "양재시민의숲"],
-  },
-];
-
 export const sections = [
   {
     departure: "교대",
