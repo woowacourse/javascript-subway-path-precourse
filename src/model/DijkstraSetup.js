a38fa1136120dd7e433ee07a9dbdc7a84bb2be8f
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 14:19:50 +0900

    feat : 초기에 다익스트라 객체에 역들간의 연결정보를 넣는 dijkstarSetup.js생성

---
+++ src/model/DijkstraSetup.js
@@ -0,0 +1,15 @@
+import { stationNodes } from "../stationNodes.js";
+import { Dijkstra } from "../utils/Dijkstra.js";
+
+export const dijkstraSetup = (timeOrDistance) => {
+    const dijkstra = new Dijkstra();
+
+    for (const stationNode of stationNodes) {
+        dijkstra.addEdge(
+            stationNode.startSation,
+            stationNode.endStation,
+            stationNode.selection[timeOrDistance]);
+    }
+
+    return dijkstra
+}
