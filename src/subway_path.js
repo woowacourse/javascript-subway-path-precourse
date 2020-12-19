7ae73ff2f164d3d27f7fc71afcbca39814eb6483
joyfuI <jong970105@gmail.com>
2020-12-19 15:47:26 +0900

    최단거리 길찾기 기능

--- src/subway_path.js
+++ src/subway_path.js
@@ -1,3 +1,4 @@
+import Dijkstra from "./utils/Dijkstra.js";
 import {station, line, edge} from "./init_data.js";
 import SubwayEdge from "./subway_edge.js";
 import SubwayMap from "./precourse/subway_map.js";
@@ -6,6 +7,7 @@ export default class SubwayPath {
   constructor() {
     this.subwayMap = new SubwayMap();
     this.edgeHash = this.initEdge(edge);
+    this.distanceDijkstra = this.initDistance(edge);
 
     const data = {
       station: station,
@@ -23,5 +25,38 @@ export default class SubwayPath {
       edgeHash[`${obj.target}-${obj.source}`] = edge;
     }
     return edgeHash;
+  }
+
+  // 최단거리 Dijkstra 세팅
+  initDistance(data) {
+    const dijkstra = new Dijkstra();
+    for (const obj of data) {
+      dijkstra.addEdge(obj.source, obj.target, obj.distance);
+    }
+    return dijkstra;
+  }
+
+  // 최단거리 구하기
+  findShortestDistancePath(source, target) {
+    return this.distanceDijkstra.findShortestPath(source, target);
+  }
+
+  // 경로의 총 거리와 총 소요시간 구하기
+  getTotalDistanceAndTime(path) {
+    let sumDistance = 0;
+    let sumTime = 0;
+    for (let i = 0; i < path.length - 1; i++) {
+      const key = `${path[i]}-${path[i + 1]}`;
+      const edge = this.edgeHash[key];
+      if (edge === undefined) {
+        return null;
+      }
+      sumDistance += edge.distance;
+      sumTime += edge.time;
+    }
+    return {
+      distance: sumDistance,
+      time: sumTime
+    };
   }
 }
