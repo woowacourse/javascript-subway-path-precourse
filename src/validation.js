73917afc2add094de91302b6c616f2b73f0faad7
min1378 <qwes123@naver.com>
2020-12-19 15:30:41 +0900

    fix: validation 반대로 체크하는 버그 수정

--- src/validation.js
+++ src/validation.js
@@ -1,21 +1,25 @@
 import { stationDistanceKMMap } from "./utils/data.js";
 import { errorMessage } from "./utils/const.js";
 const { ERROR_SHORT_STATION, ERROR_NONEXIST_STATION, ERROR_EQUAL_STATION_NAME } = errorMessage;
-const validateNameLength = (name) => name.trim().length < 2;
+const validateNameLength = (name) => name.trim().length > 1;
 
-const existStationName = (name) => !stationDistanceKMMap[name];
+const existStationName = (name) => stationDistanceKMMap[name];
 
 const equalStationName = (firstName, secondName) => firstName === secondName;
 
 export const stationNameValidation = (nameArray) => {
   const [departStationName, arrivalStationName] = nameArray;
   if (!nameArray.every(validateNameLength)) {
-    return alert(ERROR_SHORT_STATION);
+    alert(ERROR_SHORT_STATION);
+    return false;
   }
   if (!nameArray.every(existStationName)) {
-    return alert(ERROR_NONEXIST_STATION);
+    alert(ERROR_NONEXIST_STATION);
+    return false;
   }
   if (equalStationName(departStationName, arrivalStationName)) {
-    return alert(ERROR_EQUAL_STATION_NAME);
+    alert(ERROR_EQUAL_STATION_NAME);
+    return false;
   }
+  return true;
 };
