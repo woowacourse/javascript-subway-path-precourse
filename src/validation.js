f1a49037020d9750f9c730600d0712dde84fdeda
min1378 <qwes123@naver.com>
2020-12-19 15:02:38 +0900

    fix: 역이름을 리스트로 받도록 로직 수정

--- src/validation.js
+++ src/validation.js
@@ -1,23 +1,13 @@
 import { stationDistanceKMMap } from "./utils/data.js";
-const validateNameLength = (name) => {
-  if (name.trim().length < 2) {
-    return false;
-  }
-  return true;
-};
+const validateNameLength = (name) => name.trim().length < 2;
 
-const existStationName = (name) => {
-  if (!stationDistanceKMMap[name]) {
-    return false;
-  }
-  return true;
-};
+const existStationName = (name) => !stationDistanceKMMap[name];
 
-const stationNameValidation = (name) => {
-  if (!validateNameLength(name)) {
+const stationNameValidation = (nameArray) => {
+  if (!nameArray.every(validateNameLength)) {
     return alert("역의 이름은 두 글자 이상이어야 합니다.");
   }
-  if (!existStationName(name)) {
+  if (!nameArray.every(existStationName)) {
     return alert("존재하지 않은 역은 입력할 수 없습니다.");
   }
 };
