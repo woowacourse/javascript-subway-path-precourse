8eac5a699bc7d622bac88f11b2904bae023fe42b
unknown <dudtjr913@naver.com>
2020-12-19 14:48:40 +0900

    Update : README.md - 기능 구현 목록 추가(데이터 분리)
    <description>

--- src/model/subway.js
+++ src/model/subway.js
@@ -1,9 +1,14 @@
 import {STATION, LINE, SECTION} from '../constant/constant.js';
+import Dijkstra from '../utils/Dijkstra.js';
 
 export default class Subway {
   constructor() {
     this.station = STATION;
     this.line = LINE;
     this.section = SECTION;
+  }
+
+  findShortDistance() {
+    const dijkstra = new Dijkstra();
   }
 }
