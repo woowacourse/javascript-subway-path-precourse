94dcbf5641f6ba984c64273873a6f42c9c2b2955
min1378 <qwes123@naver.com>
2020-12-19 14:58:16 +0900

    feat: 역의 존재여부 확인 validation 구현

--- src/validation.js
+++ src/validation.js
@@ -1,3 +1,4 @@
+import { stationDistanceKMMap } from "./utils/data.js";
 const validateNameLength = (name) => {
   if (name.trim().length < 2) {
     return false;
@@ -5,8 +6,18 @@ const validateNameLength = (name) => {
   return true;
 };
 
-const stationNameValidation = (stationName) => {
-  if (!validateNameLength(stationName)) {
+const existStationName = (name) => {
+  if (!stationDistanceKMMap[name]) {
+    return false;
+  }
+  return true;
+};
+
+const stationNameValidation = (name) => {
+  if (!validateNameLength(name)) {
     return alert("역의 이름은 두 글자 이상이어야 합니다.");
+  }
+  if (!existStationName(name)) {
+    return alert("존재하지 않은 역은 입력할 수 없습니다.");
   }
 };
