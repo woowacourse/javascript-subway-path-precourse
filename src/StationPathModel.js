8896683f08c6f3088f8b2d07337c8b7280133a03
sunhpark42 <sunhpark42@gmail.com>
2020-12-19 14:42:43 +0900

    feat: find sections by line name

--- src/StationPathModel.js
+++ src/StationPathModel.js
@@ -1,5 +1,29 @@
+import Dijkstra from "./utils/Dijkstra.js";
+import { stations, lines } from "./data/defaulData.js"
+
 export default class StationPathModel {
   constructor() {
     this.option = 'shortest-distance';
+  }
+
+  findSections(name) {
+    const sections = lines.forEach((line) => {
+      if (line === name) {
+        return lines[line].sections;
+      };
+    })
+    return sections;
+  }
+
+  getShortestDistance(departure, arrival) {
+    const distance = 0;
+    const dijkstra = new Dijkstra();
+    const line = '2호선';
+    const sections = this.findSections(line);
+    sections.forEach((stationInfo) => {
+      dijkstra.addEdge()
+    });
+
+    return distance;
   }
 }
