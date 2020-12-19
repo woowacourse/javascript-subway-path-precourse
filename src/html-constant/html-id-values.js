6f3b27ed3ca019b33511bb65db52fddd1216cca6
Tanney-102 <dhxodms89@gmail.com>
2020-12-19 16:56:26 +0900

    hmlt id 및 name 상수 추가

--- src/html-constant/html-id-values.js
+++ src/html-constant/html-id-values.js
@@ -2,4 +2,5 @@ export const ID_VALUES = {
   departureStationNameInput: "departure-station-name-input",
   arrivalStationNameInput: "arrival-station-name-input",
   searchButton: "search-button",
+  result: "result",
 };
