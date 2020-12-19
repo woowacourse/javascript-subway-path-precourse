2b14986580b8dd0e5bdf075b844e043b6a7d3e8c
hyperflow <hyperflow@kakao.com>
2020-12-19 14:43:52 +0900

    refactor: 속성 이름 수정 및 prettier 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import Dijkstra from './utils/Dijkstra.js';
-import { stations, lines } from './consts/data.js';
+import Dijkstra from "./utils/Dijkstra.js";
+import { stations, lines } from "./consts/data.js";
+import { startManager } from "./component/manager.js";
 
 let stationData = [];
 let lineData = [];
@@ -11,7 +12,9 @@ const dijkstra_distance = new Dijkstra();
 const App = () => {
   initializeData();
   constructSTS();
-}
+
+  startManager();
+};
 
 const initializeData = () => {
   stationData = stations;
@@ -20,25 +23,25 @@ const initializeData = () => {
   stationData.map((station) => {
     stationToStation[station] = {};
   });
-}
+};
 
 const constructSTS = () => {
   lineData.map((line) => {
     line.sequence.map((path) => {
       stationToStation[path.deptStation][path.destStation] = {
-        distance: path.distance,
-        time: path.time
-      }
+        dist: path.dist,
+        time: path.time,
+      };
       stationToStation[path.destStation][path.deptStation] = {
-        distance: path.distance,
-        time: path.time
-      }
-      dijkstra_distance.addEdge(path.deptStation, path.destStation, path.distance);
+        dist: path.dist,
+        time: path.time,
+      };
+      dijkstra_distance.addEdge(path.deptStation, path.destStation, path.dist);
       dijkstra_time.addEdge(path.deptStation, path.destStation, path.time);
-    })
+    });
   });
-}
+};
 
 App();
 
-export default { dijkstra_distance, dijkstra_time, stationToStation };
+export { dijkstra_distance, dijkstra_time, stationToStation };
