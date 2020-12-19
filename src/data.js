51bad6749a68e94bdf6d2622ddde276d15d59593
Hyun Cheol <mlch1603@naver.com>
2020-12-19 13:21:33 +0900

    데이터 초기화

---
+++ src/data.js
@@ -0,0 +1,87 @@
+export const stations = [
+    {
+        name: '교대',
+    }, 
+    {
+        name: '강남',
+    }, 
+    {
+        name: '역삼',
+    }, 
+    {
+        name: '남부터미널',
+    }, 
+    {
+        name: '양재',
+    }, 
+    {
+        name: '양재시민의숲',
+    }, 
+    {
+        name: '매봉',
+    },
+];
+
+export const lines = [
+    {
+        name: '2호선',
+    },
+    {
+        name: '3호선',
+    },
+    {
+        name: '신분당선',
+    },
+];
+
+export const routes = [
+    {
+        line: '2호선',
+        departure: '교대',
+        arrival: '강남',
+        distance: 2,
+        time: 3,
+    },
+    {
+        line: '2호선',
+        departure: '강남',
+        arrival: '역삼',
+        distance: 2,
+        time: 3,
+    },
+    {
+        line: '3호선',
+        departure: '교대',
+        arrival: '남부터미널',
+        distance: 3,
+        time: 2,
+    },
+    {
+        line: '3호선',
+        departure: '남부터미널',
+        arrival: '양재',
+        distance: 6,
+        time: 5,
+    },
+    {
+        line: '3호선',
+        departure: '양재',
+        arrival: '매봉',
+        distance: 1,
+        time: 1,
+    },
+    {
+        line: '신분당선',
+        departure: '강남',
+        arrival: '양재',
+        distance: 2,
+        time: 8,
+    },
+    {
+        line: '신분당선',
+        departure: '양재',
+        arrival: '양재시민의숲',
+        distance: 10,
+        time: 3
+    },
+];
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { stations, lines, routes } from './data.js';
+
+export default class SubwayPath {
+    constructor() {
+        this.stations = stations;
+        this.lines = lines;
+    }
+}
