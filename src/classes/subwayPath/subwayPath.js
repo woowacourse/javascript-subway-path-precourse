3b75aff35c7c3efcb382d539983c3f5a27c679d3
swon3210 <swon3210@gmail.com>
2020-12-19 15:08:31 +0900

    feat: 출발역과 도착역을 입력받아 최소시간 경로를 조회

--- src/classes/subwayPath/subwayPath.js
+++ src/classes/subwayPath/subwayPath.js
@@ -6,4 +6,8 @@ export default class subwayPath {
   static getMinDistancePath(startStationName, endStationName) {
     return distanceDijkstra.findShortestPath(startStationName, endStationName);
   }
+
+  static getMinTimePath(startStationName, endStationName) {
+    return timeDijkstra.findShortestPath(startStationName, endStationName);
+  }
 }
