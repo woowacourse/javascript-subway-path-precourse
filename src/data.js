8c691ecc8f5b08eab458a0dd9f35ccdc745a2a2d
ddongule <mnk918p@gmail.com>
2020-12-19 14:52:31 +0900

    feat: 역 노선 구간 데이터 초기 설정

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const lines = [
+  {
+    name: "2호선",
+    stations: ["교대", "강남", "역삼"],
+    distance: [2, 2],
+    time: [3, 3],
+  },
+  {
+    name: "3호선",
+    stations: ["교대", "남부터미널", "양재", "매봉"],
+    distance: [3, 6, 1],
+    time: [2, 5, 1],
+  },
+  {
+    name: "신분당선",
+    stations: ["강남", "양재", "양재시민의숲"],
+    distance: [2, 10],
+    time: [8, 3],
+  },
+];
