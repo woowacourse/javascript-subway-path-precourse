492b9bc1c55374f7be06e8405857dfb220303cd4
min1378 <qwes123@naver.com>
2020-12-19 14:27:12 +0900

    simplify 함수명 간결하게 수정

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,9 @@
-export const getInputDepartureStationName = () => {
+export const getDepartureStationName = () => {
   const $inputDeapartureStationName = document.querySelector("#departure-station-name-input");
   return $inputDeapartureStationName.value;
 };
 
-export const getInputArrivalStationName = () => {
+export const getArrivalStationName = () => {
   const $inputArrivalStationName = document.querySelector("#arrival-station-name-input");
   return $inputArrivalStationName.value;
 };
