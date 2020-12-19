1248a8d7c5f1ab23b6c6f45d14f1986ec168bedd
da1907 <cd03252@naver.com>
2020-12-19 14:26:02 +0900

    feat: 데이터 초기 설정

---
+++ src/data.js
@@ -0,0 +1,86 @@
+export const stations = [
+  {
+    name: '교대',
+  },
+  {
+    name: '강남',
+  },
+  {
+    name: '역삼',
+  },
+  {
+    name: '남부터미널',
+  },
+  {
+    name: '양재',
+  },
+  {
+    name: '양재시민의숲',
+  },
+  {
+    name: '매봉',
+  },
+];
+
+export const lines = [
+  {
+    name: '2호선',
+  },
+  {
+    name: '3호선',
+  },
+  {
+    name: '신분당선',
+  },
+];
+
+export const lineTwo = [
+  {
+    departure: '교대',
+    arrive: '강남',
+    distance: 2,
+    time: 3,
+  },
+  {
+    departure: '강남',
+    arrive: '역삼',
+    distance: 2,
+    time: 3,
+  },
+];
+
+export const lineThree = [
+  {
+    departure: '교대',
+    arrive: '남부터미널',
+    distance: 3,
+    time: 2,
+  },
+  {
+    departure: '남부터미널',
+    arrive: '양재',
+    distance: 6,
+    time: 5,
+  },
+  {
+    departure: '양재',
+    arrive: '매봉',
+    distance: 1,
+    time: 1,
+  },
+];
+
+export const SinbundangLine = [
+  {
+    departure: '강남',
+    arrive: '양재',
+    distance: 2,
+    time: 8,
+  },
+  {
+    departure: '양재',
+    arrive: '양재시민의숲',
+    distance: 10,
+    time: 3,
+  },
+];
