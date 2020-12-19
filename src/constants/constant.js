14a3d57049254f96556772e3817431a2810bfc47
ChayanRin <k852012@naver.com>
2020-12-19 14:14:54 +0900

    feat: isAvailable 구현

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,7 @@
 const message = {
   ALERT_NOT_ENOUGH_LENGTH: "입력한 역명의 길이가 2글자 미만입니다",
   ALERT_NAME_EQUAL: "입력한 출발역과 도착역이 동일합니다",
+  ALERT_NOT_IN_STATIONS: "입력한 역명이 지하철 역에 존재하지 않습니다",
 };
 
 const template = {};
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,18 +1,31 @@
-import { message } from "../constants/constant";
+import { stations } from "../datas/data.js"
+import { message } from "../constants/constant.js";
 
 class util {
   isEnoughLength = (departure, arrival) => {
     if (departure.length < 2 || arrival.length < 2) {
       return message.ALERT_NOT_ENOUGH_LENGTH;
     }
+
+    return null;
   };
 
   isDiffrentName = (departure, arrival) => {
     if (departure === arrival) => {
       return message.ALERT_NAME_EQUAL;
     }
+
+    return null;
   };
 
+  isAvailable = (departure, arrival) => {
+    if (!(stations.includes(departure) && stations.includes(arrival))){
+        return message.ALERT_NOT_IN_STATIONS;
+    }
+
+    return null;
+  };
+  
 }
 
 export const {isEnoughLength, isDiffrentName} = new util();
