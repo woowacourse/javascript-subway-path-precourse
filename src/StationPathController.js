3121069795309d09aeb60558a1e1e24c62275b03
sunhpark42 <sunhpark42@gmail.com>
2020-12-19 16:10:06 +0900

    feat: get shortest path, time, distance when search button clicked

--- src/StationPathController.js
+++ src/StationPathController.js
@@ -1,4 +1,6 @@
 import GetUserInput from "./GetUserInput.js";
+import StationPathModel from "./StationPathModel.js";
+import StationPathView from "./StationPathView.js";
 
 export default class StationPathController {
   constructor () {
@@ -18,7 +20,24 @@ export default class StationPathController {
     const userInput = new GetUserInput();
     if (userInput.isValid() !== 1) {
       alert(userInput.isValid())
+      return;
     }
-    alert("valid Input, next calculate it");
+    const stationPathModel = new StationPathModel();
+    const lines = stationPathModel.findLines(userInput.departure, userInput.arrival);
+    if (lines.length === 0) {
+      alert("두 역이 연결되어 있지 않습니다.")
+      return;
+    }
+    let path;
+    if (userInput.option === 'distance') {
+      path = stationPathModel.getShortestDistancePath(userInput.departure, userInput.arrival);
+    }
+    if (userInput.option === 'time') {
+      path = stationPathModel.getShortestTimePath(userInput.departure, userInput.arrival);
+    }
+      console.log(path)
+    const view = new StationPathView();
+    const distance = stationPathModel.getDistance(lines[0], path);
+    const time = stationPathModel.getTime(lines[0], path);
   }
 }
