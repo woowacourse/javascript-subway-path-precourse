bf0e69489f40702e51230bec53c70bfa555a189f
JongHyeon Moon <dobest27@gmail.com>
2020-12-19 13:25:00 +0900

    feat: add paths data

---
+++ src/data/paths.js
@@ -0,0 +1,38 @@
+const paths = [{
+	from: '교대',
+	to : '강남',
+	distance: 2,
+	minutes: 3
+}, {
+	from: '강남',
+	to : '역삼',
+	distance: 2,
+	minutes: 3
+}, {
+	from: '교대',
+	to : '남부터미널',
+	distance: 3,
+	minutes: 2
+}, {
+	from: '남부터미널',
+	to : '양재',
+	distance: 6,
+	minutes: 5
+}, {
+	from: '양재',
+	to : '매봉',
+	distance: 1,
+	minutes: 1
+}, {
+	from: '강남',
+	to : '양재',
+	distance: 2,
+	minutes: 8,
+}, {
+	from: '양재',
+	to : '양재시민의숲',
+	distance: 10,
+	minutes: 3,
+}];
+
+export default paths;
