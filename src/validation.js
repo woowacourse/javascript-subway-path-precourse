55565e895f8e5c2f8088c909c8cde13b7aa7d169
min1378 <qwes123@naver.com>
2020-12-19 15:05:47 +0900

    feat: 출발역과 도착역의 이름 체크 함수 구현

--- src/validation.js
+++ src/validation.js
@@ -3,11 +3,17 @@ const validateNameLength = (name) => name.trim().length < 2;
 
 const existStationName = (name) => !stationDistanceKMMap[name];
 
+const equalStationName = (firstName, secondName) => firstName === secondName;
+
 const stationNameValidation = (nameArray) => {
+  const [departStationName, arrivalStationName] = nameArray;
   if (!nameArray.every(validateNameLength)) {
     return alert("역의 이름은 두 글자 이상이어야 합니다.");
   }
   if (!nameArray.every(existStationName)) {
     return alert("존재하지 않은 역은 입력할 수 없습니다.");
+  }
+  if (equalStationName(departStationName, arrivalStationName)) {
+    return alert("출발역과 도착역이 같을 수 없습니다.");
   }
 };
