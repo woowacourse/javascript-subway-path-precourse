622a6c6e90228fd36be5188f2584564302caf4c6
Kahn <wooseopim93@gmail.com>
2020-12-19 17:46:47 +0900

    fix: 자료구조 수정

--- src/data.js
+++ src/data.js
@@ -1,164 +1,35 @@
-export const stations = [
-  {
-    name: "교대",
-    lines: ["2호선", "3호선"],
-    transfer: true,
-    prev: null,
-    next: [
-      {
-        lines: "2호선",
-        distance: 2,
-        time: 3
-      },
-      {
-        lines: "3호선",
-        distance: 3,
-        time: 2
-      },
-    ]
-  },
-  {
-    name: "강남",
-    lines: ["2호선", "신분당선"],
-    transfer: true,
-    prev: [
-      {
-        lines: "2호선",
-        distance: 2,
-        time: 3
-      },
-      {
-        lines: "신분당선",
-        distance: 0,
-        time: 0,
-      }
-    ],
-    next: [
-      {
-        lines: "2호선",
-        distance: 2,
-        time: 3
-      },
-      {
-        lines: "신분당선",
-        distance: 2,
-        time: 8
-      },
-    ]
-  },
-  {
-    name: "양재",
-    lines: ["3호선", "신분당선"],
-    transfer: true,
-    prev: [
-      {
-        lines: "3호선",
-        distance: 6,
-        time: 5
-      },
-      {
-        lines: "신분당선",
-        distance: 2,
-        time: 8
-      }
-    ],
-    next: [
-      {
-        lines: "3호선",
-        distance: 1,
-        time: 1
-      },
-      {
-        lines: "신분당선",
-        distance: 10,
-        time: 3
-      },
-    ]
-  },
-  {
-    name: "역삼",
-    lines: ["2호선"],
-    transfer: false,
-    prev: [
-      {
-        lines: "2호선",
-        distance: 2,
-        time: 3
-      }
-    ],
-    next: null
-  },
-  {
-    name: "남부터미널",
-    lines: ["3호선"],
-    transfer: false,
-    prev: [
-      {
-        lines: "3호선",
-        distance: 3,
-        time: 2
-      }
-    ],
-    next: [
-      {
-        lines: "3호선",
-        distance: 6,
-        time: 5
-      }
-    ]
-  },
-  {
-    name: "매봉",
-    lines: ["3호선"],
-    transfer: false,
-    prev: [
-      {
-        lines: "3호선",
-        distance: 1,
-        time: 1
-      }
-    ],
-    next: null
-  },
-  {
-    name: "양재시민의숲",
-    lines: ["신분당선"],
-    transfer: false,
-    prev: [
-      {
-        lines: "신분당선",
-        distance: 10,
-        time: 3
-      }
-    ],
-    next: null
-  },
+export const stationList = ["교대", "강남", "역삼", "남부터미널", "양재", "매봉", "양재시민의숲"]
+
+export const pathWithDistance = [
+  ["교대", "강남", 2],
+  ["교대", "남부터미널", 3],
+  ["강남", "교대", 2],
+  ["남부터미널", "교대", 3],
+  ["강남", "역삼", 2],
+  ["역삼", "강남", 2],
+  ["강남", "양재", 2],
+  ["양재", "강남", 2],
+  ["남부터미널", "양재", 6],
+  ["양재", "남부터미널", 6],
+  ["양재", "매봉", 1],
+  ["매봉", "양재", 1],
+  ["양재", "양재시민의숲", 10],
+  ["양재시민의숲", "양재", 10]
 ]
 
-export const path = [
-  {
-    name: "2호선",
-    route: {
-      0: "교대",
-      1: "강남",
-      2: "역삼"
-    }
-  },
-  {
-    name: "3호선",
-    route: {
-      0: "교대",
-      1: "남부터미널",
-      2: "양재",
-      3: "매봉"
-    }
-  },
-  {
-    name: "신분당선",
-    route: {
-      0: "강남",
-      1: "양재",
-      2: "양재시민의숲"
-    }
-  }
+export const pathWithTime = [
+  ["교대", "강남", 2],
+  ["교대", "남부터미널", 3],
+  ["강남", "교대", 2],
+  ["남부터미널", "교대", 3],
+  ["강남", "역삼", 2],
+  ["역삼", "강남", 2],
+  ["강남", "양재", 2],
+  ["양재", "강남", 2],
+  ["남부터미널", "양재", 6],
+  ["양재", "남부터미널", 6],
+  ["양재", "매봉", 1],
+  ["매봉", "양재", 1],
+  ["양재", "양재시민의숲", 10],
+  ["양재시민의숲", "양재", 10]
 ]
