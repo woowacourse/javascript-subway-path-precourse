f9b269eb3b26b8382d9fa938cfa7bac56a432c68
jinsu <spfhd753@naver.comm>
2020-12-19 14:22:21 +0900

    feat: 역 정보 작성

---
+++ src/StationData/stationData.js
@@ -0,0 +1,9 @@
+export const stationData = [
+  { V1: "교대", V2: "강남", distance: 2, time: 3 },
+  { V1: "강남", V2: "역삼", distance: 2, time: 3 },
+  { V1: "교대", V2: "남부터미널", distance: 3, time: 2 },
+  { V1: "남부터미널", V2: "양재", distance: 6, time: 5 },
+  { V1: "양재", V2: "매봉", distance: 1, time: 1 },
+  { V1: "강남", V2: "양재", distance: 2, time: 8 },
+  { V1: "양재", V2: "양재시민의숲", distance: 10, time: 3 },
+];
