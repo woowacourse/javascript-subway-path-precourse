4e3e5918e26040b9385e394b70b55389cc0b28dd
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-19 14:36:07 +0900

    add isValid,isExist,isSame

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,4 +1,4 @@
-import { setState } from "../state.js";
+import { setState, state } from "../state.js";
 
 const Functions = function () {
   this.setDepartureStation = () => {
@@ -7,12 +7,27 @@ const Functions = function () {
     ).value;
     setState("departureStation", departureStation);
   };
-
   this.setArrivalStation = () => {
     const arrivalStation = document.getElementById("arrival-station-name-input")
       .value;
     setState("arrivalStation", arrivalStation);
   };
+
+  this.isBiggerThanOne = (a) => a > 1;
+
+  this.isExist = (station) =>
+    state.stations
+      .map((station) => station.name)
+      .reduce(
+        (result, stationAtState) => result || station === stationAtState,
+        false
+      );
+
+  this.isSame = (a, b) => a === b;
 };
 
-export const { setDepartureStation, setArrivalStation } = new Functions();
+export const {
+  setDepartureStation,
+  setArrivalStation,
+  isValid,
+} = new Functions();
