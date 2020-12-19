e22402a045c7daf299a199e2085868006a72ea1c
JongHyeon Moon <dobest27@gmail.com>
2020-12-19 13:44:49 +0900

    feat: check if station is more than 2 characters on click search

---
+++ src/App.js
@@ -0,0 +1,26 @@
+export default {
+	init(selector) {
+		this.element = document.querySelector(selector);
+		this.bindEvents();
+	},
+
+	query(selector) {
+		return this.element.querySelector(selector);
+	},
+
+	bindEvents() {
+		this.query('#search-button').addEventListener('click', this.onClickSearch.bind(this));
+	},
+
+	onClickSearch() {
+		const departure = this.query('#departure-station-name-input').value;
+		const arrival = this.query("#arrival-station-name-input").value;
+
+		if(!departure || !arrival) {
+			return alert('출발역 또는 도착역을 입력해주세요.');
+		}
+		if(departure.length < 2 || arrival.length < 2) {
+			return alert('출발역 또는 도착역은 2글자 이상 입력해야 합니다.');
+		}
+	},
+}
