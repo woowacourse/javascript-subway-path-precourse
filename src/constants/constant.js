c37466186b7e69e35d98ad8d31eb03dd42107954
ChayanRin <k852012@naver.com>
2020-12-19 14:09:45 +0900

    feat: isEnoughLength 구현

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,6 @@
-const message = {};
+const message = {
+  ALERT_NOT_ENOUGH_LENGTH: "입력한 역명의 길이가 2글자 미만입니다",
+};
 
 const template = {};
 
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,11 @@
-class util {}
+import { message } from "../constants/constant";
+
+class util {
+  isEnoughLength = (departure, arrival) => {
+    if (departure.length < 2 || arrival.length < 2) {
+      return message.ALERT_NOT_ENOUGH_LENGTH;
+    }
+  };
+}
 
 export const {} = new util();
