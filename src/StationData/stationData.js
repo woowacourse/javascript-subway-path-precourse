a9d762a1e98706538e26b006189544209aa9f05c
jinsu <spfhd753@naver.comm>
2020-12-19 15:01:34 +0900

    feat: stationData에 역 이름 배열 추가

--- src/StationData/stationData.js
+++ src/StationData/stationData.js
@@ -1,4 +1,6 @@
-export const stationData = [
+export { stationData, stationNameArray };
+
+const stationData = [
   { V1: "교대", V2: "강남", distance: 2, time: 3 },
   { V1: "강남", V2: "역삼", distance: 2, time: 3 },
   { V1: "교대", V2: "남부터미널", distance: 3, time: 2 },
@@ -6,4 +8,14 @@ export const stationData = [
   { V1: "양재", V2: "매봉", distance: 1, time: 1 },
   { V1: "강남", V2: "양재", distance: 2, time: 8 },
   { V1: "양재", V2: "양재시민의숲", distance: 10, time: 3 },
+];
+
+const stationNameArray = [
+  "교대",
+  "강남",
+  "역삼",
+  "남부터미널",
+  "양재",
+  "매봉",
+  "양재시민의숲",
 ];
