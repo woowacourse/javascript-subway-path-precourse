eeed6ca68802c2a61af313cfeb47a461502140a3
hyperflow <hyperflow@kakao.com>
2020-12-19 14:47:12 +0900

    feat(manager): 타입에 맞춰 다익스트라를 수행하고, 이를 renderer에 넘겨주는 manager 모듈

---
+++ src/component/manager.js
@@ -0,0 +1,61 @@
+import { ALERT, DOMTag } from "../consts/consts.js";
+import { renderer } from "./renderer.js";
+
+import {
+  dijkstra_distance,
+  dijkstra_time,
+  stationToStation,
+} from "../index.js";
+
+export const startManager = () => {
+  const findingPathButton = document.querySelector(
+    DOMTag.FINDING_PATH_BUTTON_QUERY
+  );
+  findingPathButton.addEventListener("click", buttonHandler);
+};
+
+const buttonHandler = () => {
+  let type;
+  const radioButtons = document.getElementsByName(DOMTag.RADIO_BUTTON_NAME);
+  for (let idx = 0; idx < radioButtons.length; idx++) {
+    if (radioButtons[idx].checked) type = radioButtons[idx].value;
+  }
+
+  const deptStation = document.querySelector(DOMTag.DEPARTURE_INPUT_QUERY)
+    .value;
+  const destStation = document.querySelector(DOMTag.DESTINATION_INPUT_QUERY)
+    .value;
+  if (buttonValidator(type, deptStation, destStation)) {
+    const dijkstraData = runDijkstra(type, deptStation, destStation);
+    renderer(type, dijkstraData);
+  }
+};
+
+const buttonValidator = (value, dept, dest) => {
+  if (!value) alert(ALERT.SELECT_RADIO_BUTTON);
+  else return true;
+
+  return false;
+};
+
+export const runDijkstra = (type, dept, dest) => {
+  let path;
+  if (type === "distance")
+    path = dijkstra_distance.findShortestPath(dept, dest);
+  else if (type === "time") path = dijkstra_time.findShortestPath(dept, dest);
+
+  return [...totalPathData(path), path];
+};
+
+const totalPathData = (path) => {
+  console.log(stationToStation);
+
+  let time = 0,
+    dist = 0;
+  for (let idx = 0; idx < path.length - 1; idx++) {
+    time += stationToStation[path[idx]][path[idx + 1]].time;
+    dist += stationToStation[path[idx]][path[idx + 1]].dist;
+  }
+
+  return [dist, time];
+};
---
+++ src/consts/consts.js
@@ -0,0 +1,17 @@
+export const DOMTag = {
+  FINDING_PATH_BUTTON_QUERY: "#search-button",
+  RESULT_CONTAINER_QUERY: "#result-container",
+  DEPARTURE_INPUT_QUERY: "#departure-station-name-input",
+  DESTINATION_INPUT_QUERY: "#arrival-station-name-input",
+
+  RADIO_BUTTON_NAME: "search-type",
+};
+
+export const ALERT = {
+  SELECT_RADIO_BUTTON: "최단거리와 최소시간 중 하나를 선택해주세요!",
+};
+
+export const STRING = {
+  TOTAL_DIST: "총 거리",
+  TOTAL_TIME: "총 소요 시간",
+};
