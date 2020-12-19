a1dad83069a40715843eda148711a7cc5600f725
yujo <yujo@student.42seoul.kr>
2020-12-19 14:34:10 +0900

    refactor: 폴더 이름 변경
    event -> controller

--- src/controller/input.js
+++ src/controller/input.js
@@ -1,7 +1,10 @@
 import { station } from '../data/station.js';
+import { render } from '../render/render.js';
 
-const checkValidLine = () => {
-  //
+const MIN_STATION_LENGTH = 2;
+
+const checkLength = (inputStations) => {
+  return inputStations.every((name) => name.length >= MIN_STATION_LENGTH);
 };
 
 const checkExistStation = (inputStations) => {
@@ -15,25 +18,25 @@ const checkDuplicate = (inputStations) => {
   return true;
 };
 
-const getInputStations = () => {
-  const startStation = document.querySelector('#departure-station-name-input');
-  const endStation = document.querySelector('#arrival-station-name-input');
-  return [startStation.value, endStation.value];
-};
-
 const checkInputValue = () => {
   const inputStations = getInputStations();
 
+  if (!checkLength(inputStations)) {
+    return alert(`역 이름은 최소 ${MIN_STATION_LENGTH}글자 이상이어야 합니다.`);
+  }
   if (!checkDuplicate(inputStations)) {
     return alert('출발역과 도착역은 다른 역을 선택해야 합니다.');
   }
   if (!checkExistStation(inputStations)) {
     return alert('존재하지 않는 역은 입력할 수 없습니다.');
   }
-  if (!checkValidLine(inputStations)) {
-    return alert('연결되지 않은 노선은 조회할 수 없습니다.');
-  }
-  return console.log('ok');
+  return render(inputStations);
+};
+
+const getInputStations = () => {
+  const startStation = document.querySelector('#departure-station-name-input');
+  const endStation = document.querySelector('#arrival-station-name-input');
+  return [startStation.value, endStation.value];
 };
 
 export const input = () => {
