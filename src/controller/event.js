33b93c960798704804692326194758a7fdad9a3c
unknown <dudtjr913@naver.com>
2020-12-19 15:59:46 +0900

    Feat : 유저가 입력한 역을 기준으로 길찾기 시작
    <description>

--- src/controller/event.js
+++ src/controller/event.js
@@ -3,8 +3,17 @@ import {isInputValid} from './valid.js';
 import {subway} from '../index.js';
 
 export const onFindRoute = () => {
-  if (isInputValid($departureName.value, $arrivalName.value, subway.station)) {
-    checkSelectedRadio();
+  const departure = $departureName.value;
+  const arrival = $arrivalName.value;
+  if (isInputValid(departure, arrival, subway.station)) {
+    const checkedValue = checkSelectedRadio();
+    if (checkedValue === '최단거리') {
+      const shortDistance = subway.findShortDistance(departure, arrival);
+      return addResultScreen(shortDistance);
+    }
+    const shortTime = subway.findShortTime(departure, arrival);
+
+    return addResultScreen(shortTime);
   }
 };
 
