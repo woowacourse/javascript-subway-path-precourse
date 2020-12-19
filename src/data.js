25ed208d960764d1ab28f6ad72ab0cfa2f2a317d
seojihwan <seojihwan213@naver.com>
2020-12-19 13:17:11 +0900

    feat: 역, 노선, 구간 데이터 초기 설정

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const stations = [
+  {
+    name: '교대',
+  },
+  {
+    name: '강남',
+  },
+  {
+    name: '역삼',
+  },
+  {
+    name: '남부터미널',
+  },
+  {
+    name: '양재',
+  },
+  {
+    name: '양재시민의숲',
+  },
+  {
+    name: '매봉 ',
+  },
+];
+
+export const lines = [
+  {
+    name: '2호선',
+    stations: [{ name: '교대' }, { name: '강남' }, { name: '역삼' }],
+    sections: [
+      { distance: '2km', time: '3분' },
+      { distance: '2km', time: '3분' },
+    ],
+  },
+  {
+    name: '3호선',
+    stations: [
+      { name: '교대' },
+      { name: '남부터미널' },
+      { name: '양재' },
+      { name: '매봉' },
+    ],
+    sections: [
+      { distance: '3km', time: '2분' },
+      { distance: '6km', time: '5분' },
+      { distance: '1km', time: '1분' },
+    ],
+  },
+  {
+    name: '신분당선',
+    stations: [{ name: '강남' }, { name: '양재' }, { name: '양재시민의숲' }],
+    sections: [
+      { distance: '2km', time: '8분' },
+      { distance: '10km', time: '3분' },
+    ],
+  },
+];
