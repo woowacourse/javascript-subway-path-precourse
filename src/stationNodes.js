ab09a36646d54a6fe7303981d0887db3e2e1d4af
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 14:18:01 +0900

    refactor : stationNodes.js에서 입력값을 한글에서 영어로 변경

--- src/stationNodes.js
+++ src/stationNodes.js
@@ -1,11 +1,11 @@
 import { stationNode } from "./stationNode.js"
 
 export const stationNodes = [
-    new stationNode("교대", "강남", { 시간: 3, 거리: 2 }),
-    new stationNode("강남", "역삼", { 시간: 3, 거리: 2 }),
-    new stationNode("교대", "남부터미널", { 시간: 2, 거리: 3 }),
-    new stationNode("남부터미널", "양재", { 시간: 5, 거리: 6 }),
-    new stationNode("양재", "매봉", { 시간: 1, 거리: 1 }),
-    new stationNode("강남", "양재", { 시간: 8, 거리: 2 }),
-    new stationNode("양재", "양재시민의 숲", { 시간: 3, 거리: 10 })
+    new stationNode("교대", "강남", { time: 3, distance: 2 }),
+    new stationNode("강남", "역삼", { time: 3, distance: 2 }),
+    new stationNode("교대", "남부터미널", { time: 2, distance: 3 }),
+    new stationNode("남부터미널", "양재", { time: 5, distance: 6 }),
+    new stationNode("양재", "매봉", { time: 1, distance: 1 }),
+    new stationNode("강남", "양재", { time: 8, distance: 2 }),
+    new stationNode("양재", "양재시민의 숲", { time: 3, distance: 10 })
 ];
