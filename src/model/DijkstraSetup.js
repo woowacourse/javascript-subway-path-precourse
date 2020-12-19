6ab71696674082d6209b2c85b121ba6868a026f4
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 15:22:50 +0900

    feat : 다익스트라 초기 데이터 설정해서 다익스트라 객체 반환하는 DijkstarSetup.js생성

--- src/model/DijkstraSetup.js
+++ src/model/DijkstraSetup.js
@@ -1,14 +1,11 @@
 import { stationNodes } from "../stationNodes.js";
-import { Dijkstra } from "../utils/Dijkstra.js";
+import Dijkstra from "../utils/Dijkstra.js";
 
 export const dijkstraSetup = (timeOrDistance) => {
     const dijkstra = new Dijkstra();
 
     for (const stationNode of stationNodes) {
-        dijkstra.addEdge(
-            stationNode.startSation,
-            stationNode.endStation,
-            stationNode.selection[timeOrDistance]);
+        dijkstra.addEdge(stationNode.startStation, stationNode.endStation, stationNode.selection[timeOrDistance]);
     }
 
     return dijkstra
