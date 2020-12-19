3bd5896e55ce2ca046b2431fe54cb3d253f75cc6
swon3210 <swon3210@gmail.com>
2020-12-19 13:12:11 +0900

    feat: project setup

---
+++ data/data.js
@@ -0,0 +1,7 @@
+import Store from '../classes/store/store.js';
+
+export const store = new Store();
+
+export default {
+  store,
+};
---
+++ templates/template.js
@@ -0,0 +1,5 @@
+export const getTemplate = () => {};
+
+export default {
+  getTemplate,
+};
