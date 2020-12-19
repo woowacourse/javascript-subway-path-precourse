2af1a75db9dedb8a8e040038ed735ed923613896
cuza22 <yeji.woo97@gmail.com>
2020-12-19 17:04:22 +0900

    예외사항 isValid 함수 생성

---
+++ src/inputValid.js
@@ -0,0 +1,22 @@
+export function isNameShort(name) {
+    return (name<2);
+}
+
+export function isStationAvaliable(name, data) {
+    let result = false;
+    for (let i=0; i<data.length; i++) {
+        if (data[i].stations.includes(name)){
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+export function areStationsDifferent(departure,arrival) {
+    return (departure!==arrival);
+}
+
+export function areStationsLinked(path) {
+    return (path!==false);
+}
