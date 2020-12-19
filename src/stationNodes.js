9f5f73afa40a633e901c7353902eefd8da979053
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 15:24:43 +0900

    refactor : new 다음에 소문자를 대문자로 수정

--- src/stationNodes.js
+++ src/stationNodes.js
@@ -1,11 +1,11 @@
-import { stationNode } from "./stationNode.js"
+import { StationNode } from "./stationNode.js"
 
 export const stationNodes = [
-    new stationNode("교대", "강남", { time: 3, distance: 2 }),
-    new stationNode("강남", "역삼", { time: 3, distance: 2 }),
-    new stationNode("교대", "남부터미널", { time: 2, distance: 3 }),
-    new stationNode("남부터미널", "양재", { time: 5, distance: 6 }),
-    new stationNode("양재", "매봉", { time: 1, distance: 1 }),
-    new stationNode("강남", "양재", { time: 8, distance: 2 }),
-    new stationNode("양재", "양재시민의 숲", { time: 3, distance: 10 })
+    new StationNode("교대", "강남", { time: 3, distance: 2 }),
+    new StationNode("강남", "역삼", { time: 3, distance: 2 }),
+    new StationNode("교대", "남부터미널", { time: 2, distance: 3 }),
+    new StationNode("남부터미널", "양재", { time: 5, distance: 6 }),
+    new StationNode("양재", "매봉", { time: 1, distance: 1 }),
+    new StationNode("강남", "양재", { time: 8, distance: 2 }),
+    new StationNode("양재", "양재시민의 숲", { time: 3, distance: 10 })
 ];
