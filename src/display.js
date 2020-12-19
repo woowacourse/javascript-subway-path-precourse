0db89ce68c326d169489fe1766acdaf9582a3753
cuza22 <yeji.woo97@gmail.com>
2020-12-19 17:04:03 +0900

    프린트 func 너무 길어서 분리

--- src/display.js
+++ src/display.js
@@ -1,15 +1,27 @@
 import { RESULT } from "./constants.js";
 
-export function appendDataToTable(distance, time, pathMessage) {
+export function appendDistanceToTable(distance){
     RESULT.DISTANCE.innerText = distance;
+}
+
+export function appendTimeToTable(time){
     RESULT.TIME.innerText = time;
-    RESULT.PATH.innerText = pathMessage;
+}
+
+export function appendPathToTable(path) {
+    RESULT.PATH.innerText = path;
 }
 
 export function changeTypeTitle(type) {
-    RESULT.TYPE.innerText = type;
+    if (type==='distance'){
+        RESULT.TYPE.innerText = '최단거리';
+    }
+    if (type==='time'){
+        RESULT.TYPE.innerText = '최소시간';
+    }
 }
 
 export function display() {
+    changeTypeTitle(this.type);
     RESULT.DISPLAY.style.display = 'block';
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import { INPUT, SEARCH, SEARCH_TYPE } from "./constants.js";
 import { minPath } from "./getMinimum.js";
-import { appendDataToTable, changeTypeTitle, display } from "./display.js";
+import { display,appendDistanceToTable,appendPathToTable,appendTimeToTable } from "./display.js";
 import { lines } from "./data.js";
 import { createDistanceMessage, createResultPathMessage, createTimeMessage } from "./createMessages.js";
 import { totalBetweenStations } from "./getTotal.js";
 
 export default function App() {
   this.data = lines;
-
   SEARCH.BUTTON.addEventListener("click", (e) => {
-    let type;
     display();
-    if (SEARCH.TYPE[0].checked == true) {
-      type = SEARCH.TYPE[0];
-      changeTypeTitle(SEARCH_TYPE.PATH);
-    }
-    if (SEARCH.TYPE[1].checked == true) {
-      type = SEARCH.TYPE[1];
-      changeTypeTitle(SEARCH_TYPE.TIME);
-    }
-    this.path = minPath(this.data, INPUT.DEPARTURE.value, INPUT.ARRIVAL.value, type.value);
-    // console.log(this.data, INPUT.DEPARTURE.value, INPUT.ARRIVAL.value, type.value);
-    console.log(createDistanceMessage(totalBetweenStations(this.data, this.path, "distance")));
-    console.log(createTimeMessage(totalBetweenStations(this.data, this.path, "time")));
-    console.log(createResultPathMessage(this.path));
-    appendDataToTable(createDistanceMessage(totalBetweenStations(this.data, this.path, "distance")),createTimeMessage(totalBetweenStations(this.data, this.path, "time")),createResultPathMessage(this.path));
+    this.type = changeType();
+    this.path = minPath(this.data, INPUT.DEPARTURE.value, INPUT.ARRIVAL.value, this.type.value);
+    appendDistanceToTable(createDistanceMessage(totalBetweenStations(this.data, this.path, "distance")));
+    appendTimeToTable(createTimeMessage(totalBetweenStations(this.data, this.path, "time")));
+    appendPathToTable(createResultPathMessage(this.path));
 });
+}
+
+function changeType() {
+  let type;
+  if (SEARCH.TYPE[0].checked == true) {
+    type = SEARCH.TYPE[0];
+  }
+  if (SEARCH.TYPE[1].checked == true) {
+    type = SEARCH.TYPE[1];
+  }
+  return type;
 }
 
 new App();
