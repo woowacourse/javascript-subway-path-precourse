44226325fe3e8c4b91646a449eb8fda2605f7db7
2SOOY <1401660@naver.com>
2020-12-19 16:55:52 +0900

    Refactor - 사용하지 않는 코드 삭제

--- src/data/subwayStore.js
+++ src/data/subwayStore.js
@@ -8,10 +8,3 @@ sections.forEach(({ departure, arrival, distance, time }) => {
   minDistanceStore.addEdge(departure, arrival, distance);
   minTimeStore.addEdge(departure, arrival, time);
 });
-
-// export const updateStore = (sections, distanceStore, timeStore) => {
-//   sections.forEach(({ departure, arrival, distance, time }) => {
-//     distanceStore.addEdge(departure, arrival, distance);
-//     timeStore.addEdge(departure, arrival, time);
-//   });
-// };
