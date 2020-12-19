6742d41a01033061866233fdfcb16fad8ab2d590
dande <7bdande@gmail.com>
2020-12-19 17:13:47 +0900

    fix: [ValidateSubwayManager] 구간 유효성 검증 삭제
    구간 유효성 검증은 해당 시점에서 할 게 아니라고 판단해서 삭제

--- src/components/ValidateSubwayManager.js
+++ src/components/ValidateSubwayManager.js
@@ -46,14 +46,6 @@ class ValidateSubwayManager {
     );
   };
 
-  isValidStationsSection = () => {
-    return isValidSection(
-      this.$arrivalStationInput,
-      this.departureStation,
-      this.arrivalStation,
-      this.lines
-    );
-  };
 
   checkNameValidation = () => {
     const userState = this.getState();
@@ -65,7 +57,6 @@ class ValidateSubwayManager {
     if (!this.isValidDepartureStation()) return false;
     if (!this.isValidArrivalStation()) return false;
     if (this.isDuplicatedStation()) return false;
-    if (!this.isValidStationsSection()) return false;
 
     return true;
   };
