5a145e26af61838e922efd1a44af84438410bd1d
jho2301 <jho2301@gmail.com>
2020-12-19 14:18:36 +0900

    feat: 데이터 초기값을 설정한다

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import Component from '../core/component.js';
 import State from '../core/state.js';
+import { lines } from '../data/lines.js';
+import { stations } from '../data/stations.js';
 import RouteInput from './route-input.js';
 import RouteResult from './route-result.js';
 
 class App extends Component {
   #searchRequest;
+  #stations;
+  #lines;
 
   constructor($target) {
     super($target);
@@ -14,6 +18,8 @@ class App extends Component {
 
   initializeStates() {
     this.#searchRequest = new State({});
+    this.#stations = stations;
+    this.#lines = lines;
   }
 
   mountTemplate() {
@@ -32,7 +38,8 @@ class App extends Component {
       '#route-result-container'
     );
     const searchRequest = this.#searchRequest;
-    new RouteInput($RouteInputContainer, { searchRequest });
+    const stations = this.#stations;
+    new RouteInput($RouteInputContainer, { searchRequest, stations });
     new RouteResult($routeResultContainer, { searchRequest });
   }
 }
---
+++ src/data/lines.js
@@ -0,0 +1,34 @@
+export const lines = [
+  {
+    lineName: '2호선',
+    sections: [
+      '교대',
+      { distance: 2, time: 3 },
+      '강남',
+      { distance: 2, time: 3 },
+      '역삼',
+    ],
+  },
+  {
+    lineName: '3호선',
+    sections: [
+      '교대',
+      { distance: 3, time: 2 },
+      '남부터미널',
+      { distance: 6, time: 5 },
+      '양재',
+      { distance: 1, time: 1 },
+      '매봉',
+    ],
+  },
+  {
+    lineName: '신분당선',
+    sections: [
+      '강남',
+      { distance: 2, time: 8 },
+      '양재',
+      { distance: 10, time: 3 },
+      '양재시민의숲',
+    ],
+  },
+];
