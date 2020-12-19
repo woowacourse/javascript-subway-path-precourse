bc2143dd20279d0c95d5d7df11a26da8a0f0db64
swon3210 <swon3210@gmail.com>
2020-12-19 15:08:05 +0900

    feat: 출발역과 도착역을 입력받아 최단 경로를 조회

--- src/classes/subwayPath/subwayPath.js
+++ src/classes/subwayPath/subwayPath.js
@@ -2,4 +2,8 @@ import DijstraStore from '../../store/dijkstra.js';
 
 const { distanceDijkstra, timeDijkstra } = DijstraStore;
 
-export default class subwayPath {}
+export default class subwayPath {
+  static getMinDistancePath(startStationName, endStationName) {
+    return distanceDijkstra.findShortestPath(startStationName, endStationName);
+  }
+}
