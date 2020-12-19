0340f328e89484dba2a557faa60522054622f490
Shim MunSeong <Puterism.K@gmail.com>
2020-12-19 16:37:03 +0900

    feat: 출발역, 도착역과 `최단거리` 혹은 `최소거리` 옵션 선택을 입력받는 기능

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -2,6 +2,13 @@ import StationModel from '../models/StationModel.js';
 import LineModel from '../models/LineModel.js';
 import Dijkstra from '../utils/Dijkstra.js';
 import { lines, stations, paths } from '../data.js';
+import { MINIMUM_INPUT_LENGTH } from '../constants.js';
+import {
+  INVALID_NAME_LENGTH_MESSAGE,
+  NO_STATION_DATA_MESSAGE,
+  SAME_STATION_MESSAGE,
+  INVALID_SEARCH_TYPE,
+} from '../messages.js';
 
 export default class AppController {
   constructor() {
@@ -11,8 +18,12 @@ export default class AppController {
       shortestDistance: new Dijkstra(),
       minimalTime: new Dijkstra(),
     };
+    this.elements = {
+      subwayPathFinderForm: document.querySelector('#subway-path-finder-form'),
+    };
 
     this.initializePath(paths);
+    this.setEventListener();
   }
 
   initializePath(initialPaths) {
@@ -22,5 +33,88 @@ export default class AppController {
       this.graphs.shortestDistance.addEdge(from, to, distance);
       this.graphs.minimalTime.addEdge(from, to, time);
     });
+  }
+
+  getFormData() {
+    const formElements = {
+      departureStationNameInput: document.querySelector('#departure-station-name-input'),
+      arrivalStationNameInput: document.querySelector('#arrival-station-name-input'),
+      searchTypeInput: document.querySelector('input[name="search-type"]:checked'),
+    };
+
+    const formData = {
+      departureStationName: formElements.departureStationNameInput.value,
+      arrivalStationName: formElements.arrivalStationNameInput.value,
+      searchType: formElements.searchTypeInput.value,
+    };
+
+    return formData;
+  }
+
+  isExistStation(name) {
+    return this.stationModel.data.includes(name);
+  }
+
+  validateSearchType(type) {
+    if (!(type === 'shortest-distance' || type === 'minimum-time')) {
+      throw new Error(INVALID_SEARCH_TYPE);
+    }
+  }
+
+  validateStations(departureStationName, arrivalStationName) {
+    if (departureStationName.length < MINIMUM_INPUT_LENGTH || arrivalStationName.length < MINIMUM_INPUT_LENGTH) {
+      throw new Error(INVALID_NAME_LENGTH_MESSAGE);
+    }
+
+    if (!this.isExistStation(departureStationName) || !this.isExistStation(arrivalStationName)) {
+      throw new Error(NO_STATION_DATA_MESSAGE);
+    }
+
+    if (departureStationName === arrivalStationName) {
+      throw new Error(SAME_STATION_MESSAGE);
+    }
+  }
+
+  validateFormData(formData) {
+    const { departureStationName, arrivalStationName, searchType } = formData;
+
+    try {
+      this.validateSearchType(searchType);
+      this.validateStations(departureStationName, arrivalStationName);
+    } catch (err) {
+      alert(err.message);
+      return false;
+    }
+
+    return true;
+  }
+
+  findShortestDistancePath() {
+    console.log('findShortestDistancePath');
+  }
+
+  findMinimumTimePath() {
+    console.log('findMinimumTimePath');
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = this.getFormData();
+    const { searchType } = formData;
+
+    if (!this.validateFormData(formData)) {
+      return;
+    }
+
+    if (searchType === 'shortest-distance') {
+      this.findShortestDistancePath();
+    } else if (searchType === 'minimum-time') {
+      this.findMinimumTimePath();
+    }
+  }
+
+  setEventListener() {
+    this.elements.subwayPathFinderForm.addEventListener('submit', this.handleSubmit.bind(this));
   }
 }
---
+++ src/messages.js
@@ -0,0 +1,6 @@
+import { MINIMUM_INPUT_LENGTH } from './constants.js';
+
+export const INVALID_NAME_LENGTH_MESSAGE = `역 이름은 ${MINIMUM_INPUT_LENGTH}글자 이상 입력해주세요`;
+export const NO_STATION_DATA_MESSAGE = '존재하지 않는 역입니다';
+export const SAME_STATION_MESSAGE = '출발역과 도착역을 서로 다르게 입력해주세요';
+export const INVALID_SEARCH_TYPE = '검색 타입을 선택해주세요';
