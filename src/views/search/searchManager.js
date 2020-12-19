19066e93f5944384acd487a05cb3d73015ad176f
shinsehantan <shinsehantan@users.noreply.github.com>
2020-12-19 15:54:04 +0900

    총 거리를 계산한다.

--- src/views/search/searchManager.js
+++ src/views/search/searchManager.js
@@ -12,8 +12,48 @@ function onSearchBtnHandler() {
 
   timeTableData.minPath = makeMinPathResult(checkedRadioInput, departure, arrival);
 
+  let totalDistance = makeTotalDistance(timeTableData.minPath);
+  // let totalTime = makeTotalTime(timeTableData.minPath);
+
+  // 최단 거리 minPath 를 이용하여 totalDistance, totalTime 계산하고 렌더링
+
+  console.log(timeTableData);
   render(app(timeTableData));
 }
+
+function makeTotalDistance(minPath) {
+  let subwayDatas = JSON.parse(localStorage.getItem("subwayDatas"));
+
+  let distance = 0;
+  for (let i = 0; i < minPath.length - 1; i++) {
+    subwayDatas.sections.forEach((section) => {
+      let conditionOne = section.depart === minPath[i] && section.end === minPath[i + 1];
+      let conditionTwo = section.end === minPath[i] && section.depart === minPath[i + 1];
+      if (conditionOne || conditionTwo) {
+        distance += Number(section.distance);
+      }
+    });
+  }
+
+  return distance;
+}
+
+// function makeTotalDistance(minPath) {
+//     let subwayDatas = JSON.parse(localStorage.getItem("subwayDatas"));
+
+//     let distance = 0;
+//     for (let i = 0; i < minPath.length - 1; i++) {
+//       subwayDatas.sections.forEach((section) => {
+//         let conditionOne = section.depart === minPath[i] && section.end === minPath[i + 1];
+//         let conditionTwo = section.end === minPath[i] && section.depart === minPath[i + 1];
+//         if (conditionOne || conditionTwo) {
+//           distance += Number(section.distance);
+//         }
+//       });
+//     }
+
+//     return distance;
+//   }
 
 function checkRadioInput() {
   let checkedRadioInput = "";
