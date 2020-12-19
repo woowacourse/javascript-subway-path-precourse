b061a14f22ff2ca37546420f9258f3381f763dc8
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-19 14:10:47 +0900

    add setDepartureStation,setArrivalStation

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,18 @@
-const Functions = function () {};
+import { setState } from "../state.js";
 
-export const {} = new Functions();
+const Functions = function () {
+  this.setDepartureStation = () => {
+    const departureStation = document.getElementById(
+      "departure-station-name-input"
+    ).value;
+    setState("departureStation", departureStation);
+  };
+
+  this.setArrivalStation = () => {
+    const arrivalStation = document.getElementById("arrival-station-name-input")
+      .value;
+    setState("arrivalStation", arrivalStation);
+  };
+};
+
+export const { setDepartureStation, setArrivalStation } = new Functions();
