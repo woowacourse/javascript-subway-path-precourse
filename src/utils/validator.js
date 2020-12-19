8f8c353a4a56b61cfae08cdc136f2e3191f78fba
jang-yunmi <newapache@naver.com>
2020-12-19 14:07:14 +0900

    feat: 각 역 이름 글자수 검증 기능 구현

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,5 +1,11 @@
 import { STATION } from "../constants.js";
 
 export const validator = (departure, arrival) => {
-  return true;
+  let validateResult = true;
+  if (
+    departure.length < STATION.NAME_LENGTH_LIMIT ||
+    arrival.length < STATION.NAME_LENGTH_LIMIT
+  )
+    validateResult = false;
+  return validateResult;
 };
