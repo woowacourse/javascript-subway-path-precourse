aeca386f4486b6eaf9e58143a55af54bd7db9ae6
kiminseob <kis6473@naver.com>
2020-12-19 14:37:59 +0900

    지하철 이벤트 리스너에 핸들러와, 뷰 연결

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import SubwayListener from './subway.listener.js';
+import SubwayView from './subway.view.js';
+import Subway from './subway.js';
+
+new SubwayListener(new Subway(), new SubwayView());
