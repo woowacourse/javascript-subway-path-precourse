2d3aa09ec626cdcbf43227e019198d6b95af95ba
unknown <dudtjr913@naver.com>
2020-12-19 14:33:02 +0900

    Feat : 최단거리/최소거리를 return하는 함수 구현 완료
    <description>
    radio 전체를 받아서 check된 것만 return했다.

--- src/controller/event.js
+++ src/controller/event.js
@@ -4,6 +4,13 @@ import {subway} from '../index.js';
 
 export const onFindRoute = () => {
   if (isInputValid($departureName.value, $arrivalName.value, subway.station)) {
-    console.log('good');
+    checkSelectedRadio();
   }
+};
+
+const checkSelectedRadio = () => {
+  const $allRadioInput = document.body.querySelectorAll('[name="search-type"]');
+  const checkedInput = [...$allRadioInput].find((input) => input.checked);
+
+  return checkedInput.value;
 };
