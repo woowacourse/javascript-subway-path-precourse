115bd75942ebb37e8042574e2ef8ce5feb1686e4
SEONBIN <tjsqls2003@gmail.com>
2020-12-19 16:43:31 +0900

    feat(path-finder/actions.js): add findPath by using Dijkstra

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { printLayout } from "./path-finder/templates.js";
-import { getPathFinderInput } from "./path-finder/actions.js";
+import { init } from "./path-finder/actions.js";
 
 const pathFinderInputBtn = document.getElementById("path-finder-input-button");
 
 export default function SubwayPath() {
   pathFinderInputBtn.addEventListener("click", () => {
-    console.log(getPathFinderInput());
+    init();
   });
 }
 
--- src/path-finder/actions.js
+++ src/path-finder/actions.js
@@ -1,4 +1,27 @@
+import { lines } from "./datas.js";
+import Dijkstra from "../utils/Dijkstra.js";
 import { isNull, isUnderTwo, isUndefind, isEqual } from "../utils/valid.js";
+
+const distanceDijkstra = new Dijkstra();
+const timeDijkstra = new Dijkstra();
+
+const setDijkstra = (_line) => {
+  let nowStation = _line.sections[0];
+  let prevStation = null;
+  let section = { distance: "", time: "" };
+
+  for (let i = 1; i < _line.sections.length; i++) {
+    if (typeof _line.sections[i] === "string") {
+      prevStation = nowStation;
+      nowStation = _line.sections[i];
+
+      distanceDijkstra.addEdge(prevStation, nowStation, section.distance);
+      timeDijkstra.addEdge(prevStation, nowStation, section.time);
+    } else {
+      section = _line.sections[i];
+    }
+  }
+};
 
 const isValid = (_departureStation, _arrivalStation) => {
   if (isNull(_departureStation) || isNull(_arrivalStation)) {
@@ -41,4 +64,25 @@ const getPathFinderInput = () => {
   }
 };
 
-export { getPathFinderInput };
+const findPath = (_departureStation, _arrivalStation, _searchType) => {
+  if (_searchType === "최단거리") {
+    return distanceDijkstra.findShortestPath(
+      _departureStation,
+      _arrivalStation
+    );
+  } else {
+    return timeDijkstra.findShortestPath(_departureStation, _arrivalStation);
+  }
+};
+
+const init = () => {
+  const pathFinderInput = getPathFinderInput();
+
+  if (pathFinderInput) {
+    lines.forEach((x) => setDijkstra(x));
+
+    console.log(findPath(...pathFinderInput));
+  }
+};
+
+export { init };
