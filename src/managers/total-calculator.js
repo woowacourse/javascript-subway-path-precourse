ed33e40689e45210f368f1f26b05534412346fb0
365kim <365listener@gmail.com>
2020-12-19 17:38:28 +0900

    fix: 총 거리 및 총 소요시간이 음수로 나오는 문제 해결

--- src/managers/total-calculator.js
+++ src/managers/total-calculator.js
@@ -3,30 +3,37 @@ import { LINES } from '../configuration.js';
 export const calculateTotalRequired = (path, option) => {
   let total = 0;
 
-  console.log(path);
   for (let i = 0; i < path.length - 1; i++) {
     let start = path[i];
     let end = path[i + 1];
 
-    total += findInterval(start, end, option);
+    total += traverseLine(start, end, option);
   }
   return total;
 };
 
-const findInterval = (start, end, option) => {
+const traverseLine = (start, end, option) => {
   let interval = -1;
 
-  LINES.forEach((line) => {
-    const stations = line.stations;
-    const intervals = line[option];
-
-    for (let i = 0; i < stations.length - 1; i++) {
-      if (stations[i] === start && stations[i] === end) {
-        interval = intervals[i];
-        break;
-      }
+  for (let i = 0; i < LINES.length; i++) {
+    interval = traverseStation(LINES[i], start, end, option);
+    if (interval !== -1) {
+      break;
     }
-  });
-  console.log(interval);
+  }
+  return interval;
+};
+
+const traverseStation = (line, start, end, option) => {
+  const stations = line.stations;
+  const intervals = line[option];
+  let interval = -1;
+
+  for (let i = 0; i < stations.length - 1; i++) {
+    if (stations[i] === start && stations[i + 1] === end) {
+      interval = intervals[i];
+      break;
+    }
+  }
   return interval;
 };
