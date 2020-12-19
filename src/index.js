c289c1e30207851c76244cff9a0e8c4cbaf47a9c
lulu-kang <dalkang.hy@gmail.com>
2020-12-19 15:37:37 +0900

    feat: 길찾기 버튼 onClick 함수 구현

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import { isValidLength, isValidStation, isDuplicatedStation } from "../check.js";
+import { isValidLength, isValidStation, isDuplicatedStation, checkOptionValue } from "../check.js";
 
-const getArrivalStationName = () => {
+const onClickSearchButton = (departureStationName, arrivalStationName) => {
+  const searchButton = document.querySelector("#search-button");
+  searchButton.addEventListener("click", () => {
+    console.log(departureStationName, arrivalStationName);
+  })
+}
+
+const getArrivalStationName = (departureStationName) => {
   const arrivalStationName = document.querySelector("#arrival-station-name-input");
   arrivalStationName.addEventListener("change", () => {
     if (isValidLength(arrivalStationName.value) && isValidStation(arrivalStationName.value) && !isDuplicatedStation(arrivalStationName.value)) {
-      console.log(arrivalStationName.value);
+      onClickSearchButton(departureStationName, arrivalStationName.value);
     } else {
       const ALERT_MESSAGE = "출발역과 중복되지 않는 이름을 입력해 주세요. 또는 출발역과 도착역이 연결되어 있는 역을 입력해 주세요."
       alert(ALERT_MESSAGE);
@@ -16,7 +23,7 @@ const getDepartureStationName = () => {
   const departureStationName = document.querySelector("#departure-station-name-input");
   departureStationName.addEventListener("change", () => {
     if (isValidLength(departureStationName.value) && isValidStation(departureStationName.value)) {
-      return departureStationName.value
+      getArrivalStationName(departureStationName.value);
     }
     else {
       const ALERT_MESSAGE = "노선에 존재하는 2글자 이상의 역 이름을 입력해 주세요.";
@@ -27,7 +34,6 @@ const getDepartureStationName = () => {
 
 const init = () => {
   getDepartureStationName();
-  getArrivalStationName();
 }
 
 init();
