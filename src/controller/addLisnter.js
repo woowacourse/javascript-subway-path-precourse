9d49c21b9be7c67b54bdcb925d6b6d03440074b4
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 16:49:58 +0900

    feat : 길 찾기 버튼을 누르면 결과를 계산하고 알맞은 결과를 보여주게 이벤트를 추가하는 addLisnter.js생성

--- src/controller/addLisnter.js
+++ src/controller/addLisnter.js
@@ -1,12 +1,16 @@
-import { checkAndReturnInput } from "../model/validateInput.js"
+import { checkAndReturnInput } from "../model/InputUtil.js"
 import { dijkstraSetup } from "../model/DijkstraSetup.js"
+import { calculateDistance, calculateTime } from "../model/calculator.js"
+import { makeResultHTML } from "../view/resultView.js"
 
 export const addEventToSearchButton = () => {
     document.querySelector("#search-button").addEventListener("click", () => {
         try {
             const stationInput = checkAndReturnInput();
-            const dijkstra = dijkstraSetup("time");
+            const dijkstra = dijkstraSetup(stationInput[2]);
             const result = dijkstra.findShortestPath(stationInput[0], stationInput[1]);
+
+            makeResultHTML(calculateDistance(result), calculateTime(result), result, stationInput[2]);
         } catch (error) {
             alert(error)
         }
