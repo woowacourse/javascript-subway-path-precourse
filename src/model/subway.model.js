9cc513c4664508fab0691ed7af68f645e7875111
kiminseob <kis6473@naver.com>
2020-12-19 17:22:11 +0900

    fix: model sections 객체의 distance 오탈자 수정
    - sections 객체 distance 오탈자 수정

--- src/model/subway.model.js
+++ src/model/subway.model.js
@@ -65,13 +65,13 @@ export const sections = {
   '2호선': {
     '교대': {
       '강남': {
-        'diatance': 2,
+        'distance': 2,
         'time': 3,
       },
     },
     '강남': {
       '역삼': {
-        'diatance': 2,
+        'distance': 2,
         'time': 3,
       },
     },
@@ -79,19 +79,19 @@ export const sections = {
   '3호선': {
     '교대': {
       '남부터미널': {
-        'diatance': 3,
+        'distance': 3,
         'time': 2,
       },
     },
     '남부터미널': {
       '양재': {
-        'diatance': 6,
+        'distance': 6,
         'time': 5,
       },
     },
     '양재': {
       '매봉': {
-        'diatance': 1,
+        'distance': 1,
         'time': 1,
       },
     },
@@ -99,13 +99,13 @@ export const sections = {
   '신분당선': {
     '강남': {
       '양재': {
-        'diatance': 2,
+        'distance': 2,
         'time': 8,
       },
     },
     '양재': {
       '양재시민의숲': {
-        'diatance': 10,
+        'distance': 10,
         'time': 3,
       },
     },
