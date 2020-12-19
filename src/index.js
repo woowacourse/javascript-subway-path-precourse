f962eaf8567881ea0346c5ebaee7653544622715
HyuuunjuKim <gus7wn@gmail.com>
2020-12-19 14:56:34 +0900

    :sparkles: 결과 UI 생성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { allStation } from "./line-info.js";
-
+import { dijkstraByDistance, dijkstraByTime } from "./utils/dijkstra-object.js";
 export const getSearchType = () => {
   const type = document.getElementsByName("search-type");
   if (type[0].checked === true) {
@@ -19,6 +19,29 @@ export const isExistingStation = (departureStation, arrivalStation) => {
   return false;
 };
 export const isExistingRoute = () => {};
+export const makeResultUI = () => {
+  const totalDistance = "1";
+  const totalTime = "20";
+  const route = "asd";
+  const resultTable = `<table>
+                        <thead>
+                          <tr>
+                            <th>총 거리</th>
+                            <th>총 소요 시간</th>
+                          </tr>
+                        </thead>
+                        <tbody id="result-table-content">
+                          <tr>
+                            <td>${totalDistance}</td>
+                            <td>${totalTime}</td>
+                          </tr>
+                          <tr>
+                            <td colspan="2">${route}</td>
+                          </tr>
+                        </tbody>
+                      </table>`;
+  document.getElementById("result").innerHTML = resultTable;
+};
 const btnSearch = document.getElementById("search-button");
 btnSearch.onclick = () => {
   const departureStation = document.getElementById(
@@ -27,4 +50,10 @@ btnSearch.onclick = () => {
   const arrivalStation = document.getElementById("arrival-station-name-input")
     .value;
   const searchType = getSearchType();
+  if (searchType === "minimum-distance") {
+    const dijkstra = dijkstraByDistance;
+    makeResultUI();
+  } else {
+    const dijkstra = dijkstraByDistance;
+  }
 };
