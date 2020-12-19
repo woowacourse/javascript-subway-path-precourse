d92a68ca3b1bb86fe5b8bcdffd8fec67ca56317a
dande <7bdande@gmail.com>
2020-12-19 15:15:36 +0900

    eat: [ValidateSubwayManager] 필요한 DOM 요소 세팅

--- src/components/ValidateSubwayManager.js
+++ src/components/ValidateSubwayManager.js
@@ -2,8 +2,22 @@ class ValidateSubwayManager {
   constructor(getState, getStations) {
     this.getState = getState;
     this.getStations = getStations;
+
+    this.render();
   }
 
+  setDOMElements = () => {
+    this.$departureStationInput = document.querySelector(
+      "#departure-station-name-input"
+    );
+    this.$arrivalStationInput = document.querySelector(
+      "#arrival-station-name-input"
+    );
+  };
+
+  render = () => {
+    this.setDOMElements();
+  };
 }
 
 export default ValidateSubwayManager;
