ba9ecd2073b6c10c4af95cbae768079c59c202bf
Lisboa <suna9267@gmail.com>
2020-12-19 15:42:42 +0900

    출발역과 도착역이 같은지 판단하는 함수 생성

--- src/index.js
+++ src/index.js
@@ -22,29 +22,43 @@ function isNotUsuableDepartureStation(departureStation, arrivalStation) {
     if ((stationNameList.includes(departureStation) === false) || (stationNameList.includes(arrivalStation) === false)) {
         alert("사용 가능한 역 이름(들)이 아닙니다.");
     }
+    isSameStations(departureStation, arrivalStation);
+}
+
+function isSameStations(departureStation, arrivalStation) {
+    if (departureStation === arrivalStation) {
+        alert("출발역과 도착역이 같을 수 없습니다.");
+    }
 }
 
 let findRouteButton = document.getElementById('search-route');
 findRouteButton.addEventListener('click', showResult);
 
-function showResult () {
+function showResult() {
     isUnderTwoCharacters();
     document.getElementById("result").style.display="block";
 
 }
 
+let stationsLineTwo = ['교대', ]
 
+//입력된 역을 데이터에서 찾기
+//데이터에서 찾으면 해당 역의 distance와 time을 반환하여 array에 push
+// function findData() {
+//     for (let i = 0; i < )
+// }
 
-//2호선 총거리 구하기
+// 2호선 총거리 구하기
 // function getTotalDistancelineTwo() {
+//     const dijkstra = new Dijkstra();
 //     for (let i = 0; i < lineTwo.length; i += 1) {
 
 //     }
 // }
 
-// function getTotalTime() {
+function getTotalTime() {
 
-// }
+}
 
 
 
