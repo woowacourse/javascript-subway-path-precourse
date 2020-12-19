5613f8eb2978893a56205808f962dbf78f24e463
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-19 16:55:01 +0900

    complete onSearchButtonClick

--- src/on-search-button-click.js
+++ src/on-search-button-click.js
@@ -1,2 +1,24 @@
-const onSearchButtonClick = function () {};
+import {
+  getMinimumPath,
+  isValid,
+  getMinimumCost,
+  setArrivalStation,
+  setDepartureStation,
+  getFormattedPath,
+} from "./utils/functions.js";
+import { clearResultDiv, renderResult } from "./utils/visualization.js";
+
+const onSearchButtonClick = function () {
+  setDepartureStation();
+  setArrivalStation();
+  if (!isValid()) {
+    alert("Something is wrong.");
+    return;
+  }
+  clearResultDiv();
+  const formattedMinPath = getFormattedPath(getMinimumPath());
+  const minDistance = getMinimumCost("distance");
+  const minTime = getMinimumCost("time");
+  renderResult(formattedMinPath, minDistance, minTime);
+};
 export default onSearchButtonClick;
