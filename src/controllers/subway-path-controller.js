1ce618d5d1c23ed690838beb5e778565b98f2c52
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-19 14:47:16 +0900

    Feat: 결과 테이블 만들어서 보여주기
    테이블 css도 추가함

--- src/controllers/subway-path-controller.js
+++ src/controllers/subway-path-controller.js
@@ -17,9 +17,15 @@ export default class SubwayPathController {
 		let departureStationName;
 		let arrivalStationName;
 		let radioSelect;
-		
+
 		[departureStationName, arrivalStationName, radioSelect] = [...searchInputs];
 
 		console.log(departureStationName, arrivalStationName, radioSelect);
+
+		const totalDistance = 5;
+		const totalTime = 7;
+		const totalPath = ['1', '2', '3', '4'];
+		const resultTableData = [radioSelect, totalDistance, totalTime, totalPath];
+		this.subwayPathOutput.renderResult(resultTableData);
 	}
 }
--- src/views/subway-path-output.js
+++ src/views/subway-path-output.js
@@ -1,14 +1,21 @@
+const KILOMETER = 'km';
+const MINUTE = '분';
+const ARROW = '→';
+const SHORTEST_PATH_KOR = '최단거리';
+const SHORTEST_TIME_KOR = '최소시간';
+
 export default class SubwayPathOutput {
 	constructor() {
+		this.subwayPathAppContainer = document.getElementById('app')
+
 		this.renderInputFieldContainer();
 	}
 
 	renderInputFieldContainer = () => {
-		const subwayPathAppContainer = document.getElementById('app')
 		const subwayPathInputContainer = document.createElement('div');
 
 		subwayPathInputContainer.setAttribute('id', 'subway-path-input-container');
-		subwayPathAppContainer.appendChild(subwayPathInputContainer);
+		this.subwayPathAppContainer.appendChild(subwayPathInputContainer);
 
 		this.createInputField(subwayPathInputContainer);
 	}
@@ -25,5 +32,55 @@ export default class SubwayPathOutput {
 		<lable for="shortest-time">최소시간</label><br />
 		<button id="search-button">길 찾기</button>
 		`;
+	}
+
+	renderResult = resultTableData => {
+		const resultTableContainer = document.createElement('div');
+		resultTableContainer.setAttribute('id', 'result-table-container');
+		this.subwayPathAppContainer.appendChild(resultTableContainer);
+
+		this.renderResultTable(resultTableContainer, resultTableData);
+	}
+
+	renderResultTable = (resultTableContainer, resultTableData) => {
+		let radioSelect;
+		let totalDistance;
+		let totalTime;
+		let totalPath;
+
+		[radioSelect, totalDistance, totalTime, totalPath] = [...resultTableData];
+
+		resultTableContainer.innerHTML = this.createResultType(radioSelect) + this.createResultTable(totalDistance, totalTime, totalPath);
+	}
+
+	createResultType = radioSelect => {
+		const resultType =
+		`
+		<h2>📝 결과</h2>
+		<h4>${radioSelect}</h4>
+		`;
+
+		return resultType;
+	}
+
+	createResultTable = (totalDistance, totalTime, totalPath) => {
+		const resultTable = 
+		`
+		<table>
+			<tr>
+				<th>총 거리</th>
+				<th>총 소요 시간</th>
+			</tr>
+			<tr>
+				<td>${totalDistance + KILOMETER}</td>
+				<td>${totalTime + MINUTE}</td>
+			</tr>
+			<tr>
+				<td colspan="2">${totalPath.join(ARROW)}</td>
+			</tr>
+		</table>
+		`;
+
+		return resultTable;
 	}
 }
