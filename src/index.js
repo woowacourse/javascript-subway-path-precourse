ad4b24afaf288f2f76e73cd2ea9e827cc68b7fa9
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 16:57:23 +0900

    feat : index.js에서 addLisnter.js를 import해 프로그램 시작준비

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { addEventToSearchButton } from "./controller/addLisnter.js";
+
+export default class SubwayPathManager {
+    constructor() {
+        addEventToSearchButton()
+    }
+}
+new SubwayPathManager();
