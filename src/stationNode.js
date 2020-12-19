34ff7963bd13d27352970be85c198ea4eb009849
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 14:09:15 +0900

    feat: 연결된 역을 배열로 가지는 node속성을 StationNode클래스에  추가

--- src/stationNode.js
+++ src/stationNode.js
@@ -2,6 +2,7 @@ export class StationNode {
     constructor(startStation, endStation, timeAndDistance) {
         this.startStation = startStation;
         this.endStation = endStation;
+        this.node = [startStation, endStation];
         this.timeAndDistance = timeAndDistance;
     }
 }
