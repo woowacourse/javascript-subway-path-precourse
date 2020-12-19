a44fc164f8a484502d142989c360b340b984d62a
rjwu95 <yugun000@gmail.com>
2020-12-19 16:53:40 +0900

    style: 코드 컨벤션 맞추기(for loop 시작할 때 변수 미리 선언)

--- src/test/notConnect.js
+++ src/test/notConnect.js
@@ -1,8 +1,11 @@
 import { stations, distanceDijkstra } from "../data.js";
 
-let k = 0;
-for (let i = 0; i < stations.length; i++) {
-  for (let j = i + 1; j < stations.length; j++) {
+let i,
+  j,
+  k = 1;
+
+for (i = 0; i < stations.length; i++) {
+  for (j = i + 1; j < stations.length; j++) {
     console.log(k);
     console.log(distanceDijkstra.findShortestPath(stations[i], stations[j]));
     k++;
