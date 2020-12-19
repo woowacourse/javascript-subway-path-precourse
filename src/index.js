68639a3385b6730cb0996cf8305869de6916ecfe
min1378 <qwes123@naver.com>
2020-12-19 16:26:18 +0900

    feat: 다익스트라로 최단거리 구하는 함수 구현

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 import { stationDistanceList, stationMinuteList } from "./utils/data.js";
 import { getDepartureStationName, getArrivalStationName, getSearchType, setBtnGetDirection } from "./dom.js";
 import { stationNameValidation } from "./validation.js";
+import Dijkstra from "./utils/Dijkstra.js";
+
 export default class subwayGetDirection {
   constructor() {
     this.stationDistanceList = stationDistanceList;
     this.stationMinuteList = stationMinuteList;
     this.searchType = null;
+    this.startStation = null;
+    this.endStation = null;
+    this.route = [];
     this.init();
   }
   init() {
     setBtnGetDirection(this.getDirection.bind(this));
   }
+  startDijkstra(data) {
+    const dijkstra = new Dijkstra();
+    data.forEach(([start, end, weight]) => dijkstra.addEdge(start, end, weight));
+    this.route = dijkstra.findShortestPath(this.startStation, this.endStation);
+  }
   getDirection(e) {
     e.preventDefault();
-    const startStationName = getDepartureStationName();
-    const endStationName = getArrivalStationName();
-    if (!stationNameValidation([startStationName, endStationName])) {
+    this.startStation = getDepartureStationName();
+    this.endStation = getArrivalStationName();
+    if (!stationNameValidation([this.startStation, this.endStation])) {
       return;
     }
     this.searchType = getSearchType();
+    if (this.searchType === "distance") {
+      this.startDijkstra(this.stationDistanceList);
+    }
+    if (this.searchType === "time") {
+      this.startDijkstra(this.stationMinuteList);
+    }
   }
 }
 
