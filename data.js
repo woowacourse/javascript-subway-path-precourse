c1a5948dbc6344358f4d8d675b0f630b010f6829
lulu-kang <dalkang.hy@gmail.com>
2020-12-19 13:29:34 +0900

    feat: 데이터 초기 설정

---
+++ data.js
@@ -0,0 +1,52 @@
+export const line2 = [
+  {
+    name: "교대",
+    distance: 2,
+    time: 3
+  },
+  {
+    name: "강남",
+    distance: 2,
+    time: 3
+  },
+  {
+    name: "역삼"
+  }
+]
+
+export const line3 = [
+  {
+    name: "교대",
+    distance: 2,
+    time: 3
+  },
+  {
+    name: "남부터미널",
+    distance: 6,
+    time: 5
+  },
+  {
+    name: "양재",
+    distance: 1,
+    time: 1
+  },
+  {
+    name: "매봉" 
+  },
+]
+
+export const lineNew = [
+  {
+    name: "강남",
+    distance: 2,
+    time: 8
+  },
+  {
+    name: "양재",
+    distance: 10,
+    time: 3
+  },
+  {
+    name: "양재시민의숲" 
+  }
+]
