a381046e30a04f0627dfc23e51fdfe2aa4a7e851
JongHyeon Moon <dobest27@gmail.com>
2020-12-19 14:18:34 +0900

    feat: put initial data on dijkstra instances

--- src/models/path.js
+++ src/models/path.js
@@ -1,4 +1,17 @@
 import paths from "../data/paths.js";
+import Dijkstra from "../utils/Dijkstra.js";
+
+const dijkstraForDistance = new Dijkstra();
+const dijkstraForTime = new Dijkstra();
+
+paths.forEach(path => {
+	const { from, to, distance, minutes } = path;
+
+	dijkstraForDistance.addEdge(from, to, distance);
+	dijkstraForDistance.addEdge(to, from, distance);
+	dijkstraForTime.addEdge(from, to, minutes);
+	dijkstraForTime.addEdge(to, from, minutes);
+});
 
 export default {
 	isExistStation(name) {
