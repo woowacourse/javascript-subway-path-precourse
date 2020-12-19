05a3eb2871f9fed0c97f3a014b397739eebf50ea
hyperflow <hyperflow@kakao.com>
2020-12-19 15:39:42 +0900

    fix: 여러 정보가 들어오더라도 동작하도록 구현

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,9 +12,13 @@ export const clearContainer = (containerArray) => {
 
 export const insertRow = (table, type, element) => {
   if (type === "heading") {
-    table.innerHTML += `<tr><th>${element[0]}</th><th>${element[1]}</th></tr>`;
+    table.innerHTML += `<tr>${element
+      .map((e) => `<th>${e}</th>`)
+      .join("")}</tr>`;
   } else if (type === "data") {
-    table.innerHTML += `<tr><td>${element[0]}</td><td>${element[1]}</td></tr>`;
+    table.innerHTML += `<tr>${element
+      .map((e) => `<td>${e}</td>`)
+      .join("")}</tr>`;
   } else if (type === "path") {
     table.innerHTML += `<tr><td colspan="2">${element[0]}</td></tr>`;
   }
