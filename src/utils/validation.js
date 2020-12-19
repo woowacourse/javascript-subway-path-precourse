41888a132d105ad09a4f95a6806388e382708c75
dande <7bdande@gmail.com>
2020-12-19 14:45:31 +0900

    feat: [validation] 입력받은 역 이름을 검증하는 메서드 추가
    - isValidNameLength: 2글자 이상인지 확인
    - isValidStation: 등록된 지하철역인지 확인

---
+++ src/utils/validation.js
@@ -0,0 +1,20 @@
+import { NUMBER, ALERT } from "../@shared/constants.js";
+import { showAlertMessage } from "../@shared/domUtils.js";
+
+const isValidStationName = ($input, stationName, stationList) => {
+  const name = stationName.trim();
+  const isValidNameLength = name.length >= NUMBER.MIN_STATION_NAME_LENGTH;
+  const isValidStation = stationList.includes(name);
+
+  if (!isValidNameLength) {
+    showAlertMessage($input, ALERT.INVALID_STATION_NAME_LENGTH);
+  }
+
+  if (!isValidStation) {
+    showAlertMessage($input, ALERT.INVALID_STATION_NAME);
+  }
+
+  return isValidNameLength && isValidStation;
+};
+
+export { isValidStationName };
