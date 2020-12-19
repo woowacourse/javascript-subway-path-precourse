4b678cfa5bb51edd5f1e37ac3a33535d8bfe1233
Lisboa <suna9267@gmail.com>
2020-12-19 13:38:09 +0900

    역, 노선, 거리 ,시간 정보를 저장

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const lineTwo = [
+    {
+        station : '교대',
+        distance : 2,
+        time : 3
+    },
+    {
+        station : '강남',
+        distance : 2,
+        time : 3
+    },
+    {
+        station : '역삼'
+    }
+]
+
+export const lineThree = [
+    {
+        station : '교대',
+        distance : 3,
+        time : 2
+    },
+    {
+        station : '남부터미널',
+        distance : 6,
+        time : 5
+    },
+    {
+        station : '양재',
+        distance : 1,
+        time : 1 
+    },
+    {
+        station : '매봉'
+    }
+]
+
+export const lineBoondang = [
+    {
+        station : '강남',
+        distance : 2,
+        time : 8
+    },
+    {
+        station : '양재',
+        distance : 10,
+        time : 3
+    },
+    {
+        station : '양재시민의숲'
+    }
+]
