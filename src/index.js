c0b848cf94268280cf74c5bcaf7acae1ea73b707
Hyun Cheol <mlch1603@naver.com>
2020-12-19 13:42:52 +0900

    feat: 초기화된 데이터를 바탕으로 다익스트라 객체 생성

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import { stations, lines, routes } from './data.js';
+import Dijkstra from './utils/Dijkstra.js';
+
+const departureStationNameInput = document.getElementById('departure-station-name-input');
+const arrivalStationNameInput = document.getElementById('arrival-station-name-input');
+const searchButton = document.getElementById('search-button');
 
 export default class SubwayPath {
     constructor() {
         this.stations = stations;
         this.lines = lines;
         this.routes = routes;
+        [this.distanceDijkstra, this.timeDijkstra] = this.createDijkstras();
+
+        this.setEventListeners();
     }
+
+    setEventListeners() {
+    }
+
+    createDijkstras() {
+        const distanceDijkstra = new Dijkstra();
+        const timeDijkstra = new Dijkstra();
+        this.routes.forEach(route => {
+            distanceDijkstra.addEdge(route.departure, route.arrival, route.distance);
+            timeDijkstra.addEdge(route.departure, route.arrival, route.time);
+        });
+        return [distanceDijkstra, timeDijkstra];
+    }
+
 }
+
+new SubwayPath();
