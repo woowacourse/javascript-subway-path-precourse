67156a625d1f17837043d99d7f4e716a09c8a37a
ChayanRin <k852012@naver.com>
2020-12-19 13:57:26 +0900

    feat: getSearchPathInput 구현

--- src/views/SubwayPathView.js
+++ src/views/SubwayPathView.js
@@ -3,6 +3,16 @@ class SubwayPathView {
     this.handleButtonClick();
   }
 
+  getSearchPathInput = () => {
+    const departure = document.getElementById("departure-station-name-input")
+      .value;
+    const arrival = document.getElementById("arrival-station-name-input").value;
+    const option = document.querySelector("input[name='search-type']:checked")
+      .value;
+
+    return { departure, arrival, option };
+  };
+
   handleButtonClick = () => {
     const searchBtn = document.getElementById("search-button");
     searchBtn.addEventListener("click", this.manageSearchPath);
