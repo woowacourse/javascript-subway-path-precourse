8a31f1e9fc181cd521d58705cf5bb8945a6e8877
HyuuunjuKim <gus7wn@gmail.com>
2020-12-19 13:35:55 +0900

    :sparkles: 노선 및 역 정보 객체 생성

---
+++ src/line-info.js
@@ -0,0 +1,20 @@
+export const lineInfo = [
+  {
+    name: "2호선",
+    stationList: ["교대", "강남", "역삼"],
+    distanceInfo: [2, 2],
+    timeInfo: [3, 3],
+  },
+  {
+    name: "3호선",
+    stationList: ["교대", "남부터미널", "양재", "매봉"],
+    distanceInfo: [3, 6, 1],
+    timeInfo: [2, 5, 1],
+  },
+  {
+    name: "신분당선",
+    stationList: ["강남", "양재", "양재시민의숲"],
+    distanceInfo: [2, 10],
+    timeInfo: [8, 3],
+  },
+];
