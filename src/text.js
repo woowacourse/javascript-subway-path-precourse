67d2fc4cd3785e66db859a4757d5c2b5bcca4ee2
shinhyogeun <tls1gy2rms3@naver.com>
2020-12-19 15:26:11 +0900

    feat : 하드코딩 제거를 위한 text.js생성

---
+++ src/text.js
@@ -0,0 +1,10 @@
+export const text = {
+    EMPTY: "",
+    TWO: 2
+}
+
+export const alertText = {
+    EMPTY_INPUT: "출발역 혹은 도착역을 입력하지 않으셨습니다.",
+    SHORTER_THAN_TWO: "역의 이름은 2글자 이상이어야 합니다.",
+    START_STATION_SAME_WHIT_END: "출발역과 도착역이 같습니다."
+}
