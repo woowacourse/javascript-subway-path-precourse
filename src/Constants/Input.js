57fc34ca73ac5291ce3664e66d3be8c33d680694
bigsaigon333 <likepeppermint@gmail.com>
2020-12-19 14:20:48 +0900

    feat: Input섹션에서 사용되는 상수들(id, name, innerText)을 정의

---
+++ src/Constants/Input.js
@@ -0,0 +1,19 @@
+export const ID = {
+  DEPARTURE_STATION_NAME_INPUT: "departure-station-name-input",
+  ARRIVAL_STASTION_NAME_INPUT: "arrival-sattion-name-input",
+  SHORTEST_DISTANCE_RADIO_BUTTON: "shortest-distance-radio-button",
+  SHORTEST_TIME_RADIO_BUTTON: "shortest-time-radio-button",
+  SEARCH_BUTTON: "search-button"
+};
+
+export const NAME = {
+  SEARCH_TYPE_RADIO_BUTTON: "search-type"
+};
+
+export const INNER_TEXT = {
+  DEPARTURE_STATION_NAME_LABEL: "출발역",
+  ARRIVAL_STATION_NAME_LABEL: "도착역",
+  SHORTEST_DISTANCE_RADIO_BUTTON_LABEL: "최단거리",
+  SHORTEST_TIME_RADIO_BUTTON_LABEL: "최소거리",
+  SEARCH_BUTTON: "길 찾기"
+};
