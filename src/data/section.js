b90b3a292af28a3a07e3f7159e072ae952884ff0
yujo <yujo@student.42seoul.kr>
2020-12-19 13:46:54 +0900

    feat: 프로그램 시작시 역, 노선, 구간 데이터 초기화하는 기능

---
+++ src/data/section.js
@@ -0,0 +1,37 @@
+export const section = [
+  {
+    station: ['교대', '강남'],
+    distance: 2,
+    time: 3,
+  },
+  {
+    station: ['강남', '역삼'],
+    distance: 2,
+    time: 3,
+  },
+  {
+    station: ['교대', '남부터미널'],
+    distance: 3,
+    time: 2,
+  },
+  {
+    station: ['남부터미널', '양재'],
+    distance: 6,
+    time: 5,
+  },
+  {
+    station: ['양재', '매봉'],
+    distance: 1,
+    time: 1,
+  },
+  {
+    station: ['강남', '양재'],
+    distance: 2,
+    time: 8,
+  },
+  {
+    station: ['양재', '양재시민의숲'],
+    distance: 10,
+    time: 3,
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { station } from './data/station.js';
+import { line } from './data/line.js';
+import { section } from './data/section.js';
+
+export default function App() {
+  console.log(station);
+  console.log(line);
+  console.log(section);
+}
+
+new App();
