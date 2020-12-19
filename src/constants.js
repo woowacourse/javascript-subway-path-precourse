f305d4936cb81fd20567b390ac790d323c6332bb
jang-yunmi <newapache@naver.com>
2020-12-19 15:25:29 +0900

    feat: 가중치 선택 타입에 따른 길찾기 로직 호출 구현

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,10 @@ export const STATION = {
   NAME_LENGTH_LIMIT: 2,
   INPUT_ERROR_MESSAGE: "경로를 찾을 역 이름을 다시 입력하세요.",
 };
+
+export const EDGE_INFO = {
+  DEPARTURE: "출발역",
+  ARRIVAL: "도착역",
+  DISTANCE: "거리",
+  TIME: "시간",
+};
--- src/pathFinder.js
+++ src/pathFinder.js
@@ -1,3 +1,19 @@
+import { sections } from "./data/subwayInfo.js";
+import Dijkstra from "./utils/Dijkstra.js";
+import { EDGE_INFO } from "./constants.js";
+
+const setMapByWeightType = (map, edges, weightType) => {};
+
 export const findShortestPath = (departure, arrival) => {
-  console.log(departure, arrival);
+  const radioElems = document.getElementsByName("search-type");
+  const dijkstra = new Dijkstra();
+  if (radioElems[0].checked) {
+    for (let line in sections)
+      setMapByWeightType(dijkstra, sections[line], EDGE_INFO.DISTANCE);
+  }
+  if (radioElems[1].checked) {
+    for (let line in sections)
+      setMapByWeightType(dijkstra, sections[line], EDGE_INFO.TIME);
+  }
+  console.log(dijkstra.findShortestPath(departure, arrival));
 };
