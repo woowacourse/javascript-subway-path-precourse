da4c44cc2e7a0e173825e06db944dcb0ed1939b7
min1378 <qwes123@naver.com>
2020-12-19 15:11:31 +0900

    refactor: alert 에러메시지 상수로 변경

---
+++ src/utils/const.js
@@ -0,0 +1,5 @@
+export const errorMessage = {
+  ERROR_SHORT_STATION: "역의 이름은 두 글자 이상이어야 합니다.",
+  ERROR_NONEXIST_STATION: "존재하지 않은 역은 입력할 수 없습니다.",
+  ERROR_EQUAL_STATION_NAME: "출발역과 도착역이 같을 수 없습니다.",
+};
--- src/validation.js
+++ src/validation.js
@@ -1,20 +1,21 @@
 import { stationDistanceKMMap } from "./utils/data.js";
-
+import { errorMessage } from "./utils/const.js";
+const { ERROR_SHORT_STATION, ERROR_NONEXIST_STATION, ERROR_EQUAL_STATION_NAME } = errorMessage;
 const validateNameLength = (name) => name.trim().length < 2;
 
 const existStationName = (name) => !stationDistanceKMMap[name];
 
 const equalStationName = (firstName, secondName) => firstName === secondName;
 
-const stationNameValidation = (nameArray) => {
+export const stationNameValidation = (nameArray) => {
   const [departStationName, arrivalStationName] = nameArray;
   if (!nameArray.every(validateNameLength)) {
-    return alert("역의 이름은 두 글자 이상이어야 합니다.");
+    return alert(ERROR_SHORT_STATION);
   }
   if (!nameArray.every(existStationName)) {
-    return alert("존재하지 않은 역은 입력할 수 없습니다.");
+    return alert(ERROR_NONEXIST_STATION);
   }
   if (equalStationName(departStationName, arrivalStationName)) {
-    return alert("출발역과 도착역이 같을 수 없습니다.");
+    return alert(ERROR_EQUAL_STATION_NAME);
   }
 };
