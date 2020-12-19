6867210c318be454f201ed5289275bf50b3695e7
zereight <fullstackmachinelearning@gmail.com>
2020-12-19 14:05:55 +0900

    fix: Update data

--- src/data.js
+++ src/data.js
@@ -5,14 +5,26 @@ export const stations = [
 	},
 	{
 		name: '강남',
-		line: ['2호선', '3호선'],
+		line: ['2호선', '3호선', '신분당선'],
 	},
 	{
-		name: '양재',
+		name: '역삼',
+		line: ['2호선'],
+	},
+	{
+		name: '남부터미널',
 		line: ['3호선'],
 	},
 	{
-		name: '양재시민의숲',
+		name: '양재',
+		line: ['3호선', '신분당선'],
+	},
+	{
+		name: '양재시민의술',
+		line: ['3호선', '신분당선'],
+	},
+	{
+		name: '매봉',
 		line: ['3호선'],
 	},
 ];
@@ -22,25 +34,43 @@ export const edges = [
 	{
 		from: '교대',
 		to: '강남',
-		distance: 4,
+		distance: 2,
+		time: 3,
+	},
+	{
+		from: '강남',
+		to: '역삼',
+		distance: 2,
+		time: 3,
+	},
+	{
+		from: '교대',
+		to: '남부터미널',
+		distance: 3,
+		time: 2,
+	},
+	{
+		from: '남부터미널',
+		to: '양재',
+		distance: 6,
 		time: 5,
+	},
+	{
+		from: '양재',
+		to: '매봉',
+		distance: 1,
+		time: 1,
 	},
 	{
 		from: '강남',
 		to: '양재',
-		distance: 8,
-		time: 10,
+		distance: 2,
+		time: 8,
 	},
 	{
 		from: '양재',
 		to: '양재시민의숲',
-		distance: 2,
-		time: 2.5,
-	},
-	{
-		from: '교대',
-		to: '양재시민의숲',
-		distance: 4,
-		time: 5,
+		distance: 10,
+		time: 3,
 	},
 ];
