9cd18f04ce0f70d8e444aa0714f23df9e9c3db2d
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-12-19 15:38:18 +0900

    Feat: 역, 구간 데이터를 이용해 최단 거리를 구할 노드와 간선 생성

---
+++ src/models/subway-distance-map-model.js
@@ -0,0 +1,43 @@
+import {stations} from '../data/station-data.js';
+import {lines} from '../data/line-data.js';
+import {sections} from '../data/section-data.js';
+import Dijkstra from '../utils/Dijkstra.js';
+
+export default class SubwayDistanceMap {
+	constructor() {
+		this.subwayDistanceMap = new Dijkstra();
+
+		this.setSubwayStations();
+		this.setSubwayDistances();
+	}
+
+	getStations = () => {
+		return 	stations;
+	}
+
+	getLines = () => {
+		return lines;
+	}
+
+	getSections = () => {
+		return sections;
+	}
+
+	setSubwayStations = () => {
+		for (let station of stations) {
+			this.subwayDistanceMap.addVertex(station.name);
+		}
+	}
+
+	setSubwayDistances = () => {
+		for (let section of sections) {
+			this.subwayDistanceMap.addEdge(section.startStation, section.endStation, section.distance);
+		}
+	}
+
+	getShortestDistance = (departureStation, arrivalStation) => {
+		const shortestDistance = this.subwayDistanceMap.findShortestPath(departureStation, arrivalStation);
+		
+		return shortestDistance;
+	}
+}
