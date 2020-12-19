1f3d76942ee7d047c4e2bc35b5459ce15a96469a
ChayanRin <k852012@naver.com>
2020-12-19 13:46:32 +0900

    feat:  길 찾기 버튼 클릭 감지 handleButtonClick 구현

--- src/views/SubwayPathView.js
+++ src/views/SubwayPathView.js
@@ -1,3 +1,10 @@
 class SubwayPathView {
-  constructor() {}
+  constructor() {
+    this.handleButtonClick();
+  }
+
+  handleButtonClick = () => {
+    const searchBtn = document.getElementById("search-button");
+    searchBtn.addEventListener("click", this.manageSearchPath);
+  };
 }
