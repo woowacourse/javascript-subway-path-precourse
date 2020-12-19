1d48b774366cc12b479c8bd43f522476b7b6d8e8
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-19 14:19:09 +0900

    set search type click listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import onRadioClick from "./on-radio-click.js";
 import onSearchButtonClick from "./on-search-button-click.js";
 
 document
   .getElementById("search-button")
   .addEventListener("click", onSearchButtonClick);
+document.getElementById("distance").addEventListener("click", onRadioClick);
+const searchTypes = document.getElementsByName("search-type");
+for (let i = 0; i < searchTypes.length; i++) {
+  searchTypes[i].addEventListener("click", onRadioClick);
+}
