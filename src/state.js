9d8925fda5568cce34939d1a6481d735faf6de5f
dawit jeong <wjdekdnlt1215@gmail.com>
2020-12-19 14:00:10 +0900

    update state

--- src/state.js
+++ src/state.js
@@ -1,13 +1,47 @@
 const State = function () {
   this.state = {
-    LINE2: ["교대", "2,3", "강남", "2,3", "역삼"],
-    LINE3: ["교대", "3,2", "남부터미널", "6,5", "양재", "1,1", "매봉"],
-    SINDANG: ["강남", "2,8", "양재", "10,3", "양재시민의숲"],
+    stations: [
+      {
+        name: "교대",
+        relatedStations: [
+          { name: "강남", km: 2, minute: 3 },
+          { name: "남부터미널", km: 3, minute: 2 },
+        ],
+      },
+      {
+        name: "강남",
+        relatedStations: [
+          { name: "교대", km: 2, minute: 3 },
+          { name: "역삼", km: 2, minute: 3 },
+          { name: "양재", km: 2, minute: 8 },
+        ],
+      },
+      {
+        name: "역삼",
+        relatedStations: [{ name: "강남", km: 2, minute: 3 }],
+      },
+      {
+        name: "남부터미널",
+        relatedStations: [
+          { name: "교대", km: 3, minute: 2 },
+          { name: "양재", km: 6, minute: 5 },
+        ],
+      },
+      {
+        name: "양재",
+        relatedStations: [
+          { name: "남부터미널", km: 6, minute: 5 },
+          { name: "매봉", km: 1, minute: 1 },
+          { name: "강남", km: 2, minute: 8 },
+          { name: "양재시민의숲", km: 10, minute: 3 },
+        ],
+      },
+    ],
+    departureStation: "",
+    arrivalStation: "",
     orderBy: "distance",
-    startStation: "",
-    endStation: "",
   };
   this.setState = (key, value) = this.state[key] = value;
 };
 
-export const { state, setState };
+export const { state, setState } = new State();
