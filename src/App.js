b2a3b0a2c69148033ac91a27fbbc85826b85f4bc
Kwakcena <rhkrgudwh@gmail.com>
2020-12-19 14:10:16 +0900

    feat: Render station input
    * 출발역과 도착역을 입력할 수 있다.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ export default class App {
   constructor(target) {
     this.target = target;
     this.createHeader(target);
+    this.createStationInput(target);
   }
 
   createHeader(target) {
     const header = document.createElement('header');
     header.innerHTML = `<h1>🚇 지하철 길찾기</h1>`;
     target.appendChild(header);
+  }
+
+  createStationInput(target) {
+    const container = document.createElement('div');
+    target.appendChild(container);
+    container.className = 'station-input';
+
+    container.innerHTML = `
+      <div>
+        <label for="departure">출발역</label>
+        <input
+          type="text"
+          name="departure"
+          id="departure-station-name-input"
+        />
+      </div>
+      <div>
+        <label for="arrival">도착역</label>
+        <input
+          type="text"
+          name="arrival"
+          id="arrival-station-name-input"
+        />
+      </div>
+    `;
   }
 }
